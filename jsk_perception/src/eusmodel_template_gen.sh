9cb0f8bd44442bfefb2060f1402275878d2c5a64
manabu <manabu@jsk.t.u-tokyo.ac.jp>
2011-11-14 22:12:21 +0000

    change detection launch generation script to use jsk_perception/point_pose_extractor

--- jsk_perception/src/eusmodel_template_gen.sh
+++ jsk_perception/src/eusmodel_template_gen.sh
@@ -4,4 +4,5 @@ cd `rospack find jsk_perception`
 mkdir -p launch
 mkdir -p template
 export ROS_MASTER_URI=http://localhost:12347
-roscore -p 12347 & (roseus ./src/eusmodel_template_gen.l && kill -INT $!)
+#roscore -p 12347 & (roseus ./src/eusmodel_template_gen.l && kill -INT $!)
+roseus ./src/eusmodel_template_gen.l

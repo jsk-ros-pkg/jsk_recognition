22ba23c763a68ccd7bcc1ad43af9715007b980cd
k-okada <k-okada@jsk.t.u-tokyo.ac.jp>
2011-11-28 23:42:20 +0000

    fix to work without roseus path in PATH

--- jsk_perception/src/eusmodel_template_gen.sh
+++ jsk_perception/src/eusmodel_template_gen.sh
@@ -3,6 +3,6 @@
 cd `rospack find jsk_perception`
 mkdir -p launch
 mkdir -p template
-export ROS_MASTER_URI=http://localhost:12347
+#export ROS_MASTER_URI=http://localhost:12347
 #roscore -p 12347 & (roseus ./src/eusmodel_template_gen.l && kill -INT $!)
-roseus ./src/eusmodel_template_gen.l
+rosrun roseus roseus ./src/eusmodel_template_gen.l

fcf1d318c84ce42b9713151037173fc4bd885eb6
youhei <youhei@jsk.imi.i.u-tokyo.ac.jp>
2012-11-20 15:52:18 +0000

    delete obsolated files

--- jsk_pcl_ros/src/HSV_color_filter_nodelet.cpp
+++
@@ -1,4 +0,0 @@
-#include <pluginlib/class_list_macros.h>
-#include "jsk_pcl_ros/HSV_color_filter_nodelet.h"
-
-PLUGINLIB_DECLARE_CLASS(jsk_pcl, HSVColorFilter2, pcl_ros::HSVColorFilter2, nodelet::Nodelet);

db3dc20de6b2e02ce5d540fbbe27ce75f5573c9b
ueda <ueda@jsk.t.u-tokyo.ac.jp>
2013-10-01 10:32:14 +0000

    not subscribing topic to refer timestamp

--- jsk_pcl_ros/scripts/draw_3d_circle.py
+++ jsk_pcl_ros/scripts/draw_3d_circle.py
@@ -10,11 +10,11 @@ from std_msgs.msg import ColorRGBA
 import math
 
 def usage():
-  print "Usage: ", sys.argv[0], "id frame_id diameter timestamp_topic topic_type [r, g, b]"
+  print "Usage: ", sys.argv[0], "id frame_id diameter [r, g, b]"
   
 pub = None
-def callback(data):
-  now = data.header.stamp
+def callback():
+  now = rospy.Time.now()
   marker = ImageMarker2()
   marker.id = marker_id
   marker.header.frame_id = frame_id
@@ -43,32 +43,25 @@ def callback(data):
 def main():
   global pub, marker_id, frame_id, diameter, use_color, color
   rospy.init_node("draw_3d_circle")
-  if len(rospy.myargv()) != 6 and len(rospy.myargv()) != 9:
+  if len(rospy.myargv()) != 4 and len(rospy.myargv()) != 7:
     usage()
     exit(1)
   marker_id = int(rospy.myargv()[1])
   frame_id = rospy.myargv()[2]
   diameter = float(rospy.myargv()[3])
   pub = rospy.Publisher("image_marker", ImageMarker2)
-  timestamp_topic = rospy.myargv()[4]
-  topic_type = rospy.myargv()[5]          #like sensor_msgs/PointCloud2
   # loading module
-  topic_module = topic_type.split("/")[0]
-  topic_submodule = topic_type.split("/")[1]
-  roslib.load_manifest(topic_module)
-  #topic_type_object = getattr(__import__(topic_module + "." + "msg"), topic_submodule)
-  topic_type_object = __import__(topic_module + "." + "msg")
-  topic_type_class = getattr(topic_type_object.msg, topic_submodule)
   use_color = False
   color = ColorRGBA()
-  if len(rospy.myargv()) == 9:
+  if len(rospy.myargv()) == 7:
     use_color = True
-    color.r = float(rospy.myargv()[6])
-    color.g = float(rospy.myargv()[7])
-    color.b = float(rospy.myargv()[8])
+    color.r = float(rospy.myargv()[4])
+    color.g = float(rospy.myargv()[5])
+    color.b = float(rospy.myargv()[6])
     color.a = 1.0
-  rospy.Subscriber(timestamp_topic, topic_type_class, callback)
-  rospy.spin()
+  while not rospy.is_shutdown():
+    callback()
+    rospy.sleep(1.0)
   
 if __name__ == '__main__':
   try:

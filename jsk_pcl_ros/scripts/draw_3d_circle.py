9636af8e2cdfc3e12ff5bcf80c78298610956c13
ueda <ueda@jsk.t.u-tokyo.ac.jp>
2013-10-02 09:44:30 +0000

    providing a class

--- jsk_pcl_ros/scripts/draw_3d_circle.py
+++ jsk_pcl_ros/scripts/draw_3d_circle.py
@@ -10,59 +10,65 @@ from std_msgs.msg import ColorRGBA
 import math
 
 def usage():
-  print "Usage: ", sys.argv[0], "id frame_id diameter [r, g, b]"
+  print "Usage: ", sys.argv[0], "id frame_id radius [r, g, b]"
   
-pub = None
-def callback():
-  now = rospy.Time.now()
-  marker = ImageMarker2()
-  marker.id = marker_id
-  marker.header.frame_id = frame_id
-  marker.header.stamp = now
-  #marker.type = ImageMarker2.LINE_STRIP3D
-  marker.type = ImageMarker2.POLYGON3D
+class Drawer3DCircle:
   RESOLUTION = 20
-  point_array = PointArrayStamped()
-  point_array.header.frame_id = frame_id
-  point_array.header.stamp = now
-  for i in range(RESOLUTION + 1) + [0]:
-    # x = cos(theta), y = sin(theta)
-    theta = 2 * math.pi / RESOLUTION * i
-    x = diameter * math.cos(theta)
-    y = diameter * math.sin(theta)
-    point = Point()
-    point.x = x
-    point.y = y
-    point.z = 0
-    point_array.points.append(point)
-  marker.points3D = point_array
-  if use_color:
-    marker.outline_colors = [color]
-    marker.filled = 1
-    marker.fill_color = color
-  pub.publish(marker)
-  
+  def __init__(self, topic, marker_id, frame_id, radius, color):
+    self.topic = topic
+    self.marker_id = marker_id
+    self.frame_id = frame_id
+    self.radius = radius
+    if color:
+      self.color = ColorRGBA()
+      self.color.r = color[0]
+      self.color.g = color[1]
+      self.color.b = color[2]
+  def advertise(self):
+    self.publisher = rospy.Publisher(self.topic, ImageMarker2)
+  def publish(self):
+    marker = ImageMarker2()
+    now = rospy.Time.now()
+    marker.id = self.marker_id
+    marker.header.stamp = now
+    marker.header.frame_id = self.frame_id
+    marker.type = ImageMarker2.POLYGON3D
+    point_array = PointArrayStamped()
+    point_array.header.frame_id = self.frame_id
+    point_array.header.stamp = now
+    for i in range(self.RESOLUTION + 1) + [0]:
+      theta = 2 * math.pi / self.RESOLUTION * i
+      x = self.radius * math.cos(theta)
+      y = self.radius * math.sin(theta)
+      point = Point()
+      point.x = x
+      point.y = y
+      point.z = 0
+      point_array.points.append(point)
+    marker.points3D = point_array
+    if self.color:
+      marker.outline_colors = [self.color]
+      marker.filled = 1
+      marker.fill_color = self.color
+    self.publisher.publish(marker)
+
 def main():
-  global pub, marker_id, frame_id, diameter, use_color, color
+  # global pub, marker_id, frame_id, radius, use_color, color
   rospy.init_node("draw_3d_circle")
   if len(rospy.myargv()) != 4 and len(rospy.myargv()) != 7:
     usage()
     exit(1)
   marker_id = int(rospy.myargv()[1])
   frame_id = rospy.myargv()[2]
-  diameter = float(rospy.myargv()[3])
-  pub = rospy.Publisher("image_marker", ImageMarker2)
-  # loading module
-  use_color = False
-  color = ColorRGBA()
-  if len(rospy.myargv()) == 7:
-    use_color = True
-    color.r = float(rospy.myargv()[4])
-    color.g = float(rospy.myargv()[5])
-    color.b = float(rospy.myargv()[6])
-    color.a = 1.0
+  radius = float(rospy.myargv()[3])
+  if len(rospy.myargv()) != 7:
+    pub = Drawer3DCircle("image_marker", marker_id, frame_id, radius, None)
+  else:
+    pub = Drawer3DCircle("image_marker", marker_id, frame_id, radius, 
+                         [float(rospy.myargv()[4]), float(rospy.myargv()[5]), float(rospy.myargv()[6])])
+  pub.advertise()
   while not rospy.is_shutdown():
-    callback()
+    pub.publish()
     rospy.sleep(1.0)
   
 if __name__ == '__main__':

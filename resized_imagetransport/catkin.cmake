a3dfcfd1864555cf257a93f5c6540a75f9e6471f
k-okada <k-okada@jsk.t.u-tokyo.ac.jp>
2013-11-16 05:16:15 +0000

    catkinize resized_imagetransport

---
+++ resized_imagetransport/catkin.cmake
@@ -0,0 +1,32 @@
+# http://ros.org/doc/groovy/api/catkin/html/user_guide/supposed.html
+cmake_minimum_required(VERSION 2.8.3)
+project(resized_imagetransport)
+
+find_package(catkin REQUIRED COMPONENTS cv_bridge sensor_msgs image_transport std_srvs)
+find_package(OpenCV REQUIRED)
+
+catkin_package(
+    DEPENDS # opencv2
+    CATKIN-DEPENDS cv_bridge sensor_msgs image_transport std_srvs
+    INCLUDE_DIRS # TODO include
+    LIBRARIES # TODO
+)
+
+include_directories(${catkin_INCLUDE_DIRS})
+add_executable(image_resizer src/image_resizer.cpp)
+target_link_libraries(image_resizer ${catkin_LIBRARIES} ${OpenCV_LIBS})
+
+# Mark executables and/or libraries for installation
+install(TARGETS image_resizer
+  ARCHIVE DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION}
+  LIBRARY DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION}
+  RUNTIME DESTINATION ${CATKIN_PACKAGE_BIN_DESTINATION}
+  )
+
+
+install(DIRECTORY launch/
+        DESTINATION ${CATKIN_PACKAGE_SHARE_DESTINATION}
+        PATTERN ".svn" EXCLUDE
+        )
+
+

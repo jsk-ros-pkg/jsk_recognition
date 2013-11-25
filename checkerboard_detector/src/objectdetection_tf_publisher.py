82764a2156fd32bfd13ca85306bf38add8a0e216
kazuto <kazuto@jsk.t.u-tokyo.ac.jp>
2013-11-25 12:07:59 +0000

    update objectdetection_tf_publisher by using tf msg directly

--- checkerboard_detector/src/objectdetection_tf_publisher.py
+++ checkerboard_detector/src/objectdetection_tf_publisher.py
@@ -5,28 +5,25 @@ roslib.load_manifest("posedetection_msgs")
 roslib.load_manifest("dynamic_tf_publisher")
 from posedetection_msgs.msg import ObjectDetection
 from dynamic_tf_publisher.srv import *
+import tf
 
-def callback(msg):
+def callback(msg, subscriber):
     for detected_object in msg.objects:
-        rospy.wait_for_service('set_dynamic_tf')
-        try:
-            execute_service = rospy.ServiceProxy('set_dynamic_tf', SetDynamicTF)
-            srv = SetDynamicTFRequest()
-            srv.freq = 100
-            srv.cur_tf.transform.translation.x = detected_object.pose.position.x
-            srv.cur_tf.transform.translation.y = detected_object.pose.position.y
-            srv.cur_tf.transform.translation.z = detected_object.pose.position.z
-            srv.cur_tf.transform.rotation.x = detected_object.pose.orientation.x
-            srv.cur_tf.transform.rotation.y = detected_object.pose.orientation.y
-            srv.cur_tf.transform.rotation.z = detected_object.pose.orientation.z
-            srv.cur_tf.transform.rotation.w = detected_object.pose.orientation.w
-            srv.cur_tf.header = msg.header
-
-            response = execute_service(srv)
-        except rospy.ServiceException, e:
-            print "Service call failed: %s"%e
+        br = tf.TransformBroadcaster()
+        br.sendTransform((detected_object.position.x,
+                          detected_object.position.y,
+                          detected_object.position.z),
+                         (detected_object.orientation.x,
+                          detected_object.orientation.y,
+                          detected_object.orientation.z,
+                          detected_object.orientation.w),
+                         rospy.Time.now(),
+                         "object_" + msg.header.seq,
+                         msg.header.frame_id)
+#        subscriber.unregister()
 
 if __name__== '__main__':
     rospy.init_node('objectdetection_tf_publisher', anonymous=True)
-    rospy.Subscriber("ObjectDetection", ObjectDetection, callback)
+    subscriber = None
+    subscriber = rospy.Subscriber("ObjectDetection", ObjectDetection, callback, subscriber)
     rospy.spin()

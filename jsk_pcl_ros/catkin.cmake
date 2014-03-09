9c4bb6b3f57ca43436b9117f2afbc18e6b6d6548
Ryohei Ueda <garaemon@gmail.com>
2014-03-10 02:35:47 +0900

    #11: pcl is not a catkin package

--- jsk_pcl_ros/catkin.cmake
+++ jsk_pcl_ros/catkin.cmake
@@ -21,21 +21,13 @@ generate_dynamic_reconfigure_options(
   cfg/RGBColorFilter.cfg
   )
 
-if($ENV{ROS_DISTRO} STREQUAL "groovy")
-catkin_package(
-    CATKIN_DEPENDS pcl_ros message_runtime ${PCL_MSGS} pcl
-    INCLUDE_DIRS include
-    LIBRARIES jsk_pcl_ros
-)
-else($ENV{ROS_DISTRO} STREQUAL "groovy")
 catkin_package(
     DEPENDS pcl
     CATKIN_DEPENDS pcl_ros message_runtime ${PCL_MSGS}
     INCLUDE_DIRS include
     LIBRARIES jsk_pcl_ros
 )
-  
-endif($ENV{ROS_DISTRO} STREQUAL "groovy")
+
 #include_directories(include cfg/cpp)
 include_directories(include ${catkin_INCLUDE_DIRS})
 

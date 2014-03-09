dd66cfd7b5c88005aba84d9eae1b848d947a0610
Ryohei Ueda <garaemon@gmail.com>
2014-03-10 01:30:57 +0900

    #11: add python-rosinstall

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -3,7 +3,7 @@ echo "Testing branch $TRAVIS_BRANCH of $REPOSITORY_NAME"
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 sudo apt-get update -qq
-sudo apt-get install -qq -y python-catkin-pkg python-rosdep python-wstool ros-$ROS_DISTRO-catkin ros-$ROS_DISTRO-ros
+sudo apt-get install -qq -y python-catkin-pkg python-rosdep python-wstool ros-$ROS_DISTRO-catkin ros-$ROS_DISTRO-ros python-rosinstall
 # MongoDB hack - I don't fully understand this but its for moveit_warehouse
 sudo apt-get remove -y mongodb mongodb-10gen
 sudo apt-get install -y mongodb-clients mongodb-server -o Dpkg::Options::="--force-confdef" # default actions

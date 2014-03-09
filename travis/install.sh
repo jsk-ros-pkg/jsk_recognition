8b293b1ce9678e78abb438efb62fa509387380be
Ryohei Ueda <garaemon@gmail.com>
2014-03-10 01:48:52 +0900

    #11: fix typo ROSWS -> WSTOOL

--- travis/install.sh
+++ travis/install.sh
@@ -5,11 +5,11 @@ cd ~/ros/ws_$REPOSITORY_NAME/src
 $WSTOOL init .
 $WSTOOL merge http://raw.github.com/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/.rosinstall
 if [ $WSTOOL = rosws ] ; then
-    $ROSWS merge /opt/ros/$ROS_DISTRO/.rosinstall
+    $WSTOOL merge /opt/ros/$ROS_DISTRO/.rosinstall
 fi
 $WSTOOL update -j10
 if [ $WSTOOL = rosws ] ; then
-    $ROSWS set --git $REPOSITORY_NAME https://dummy.com/dummy -y
+    $WSTOOL set --git $REPOSITORY_NAME https://dummy.com/dummy -y
 fi
 ln -s $CI_SOURCE_PATH . # Link the repo we are testing to the new workspace
 cd ../

822057d4ee582b1c1d1b9be832328bb07a45de35
Ryohei Ueda <garaemon@gmail.com>
2014-03-10 01:36:14 +0900

    #11: add rosws/rosbuild test

--- travis/install.sh
+++ travis/install.sh
@@ -4,7 +4,14 @@ mkdir -p ~/ros/ws_$REPOSITORY_NAME/src
 cd ~/ros/ws_$REPOSITORY_NAME/src
 $WSTOOL init .
 $WSTOOL merge http://raw.github.com/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/.rosinstall
-$WSTOOL update
+if [ $WSTOOL = rosws ] ; then
+    $ROSWS merge /opt/ros/$ROS_DISTRO/.rosinstall
+fi
+$WSTOOL update -j10
+echo TRAVIS_REPO_SLUG is $TRAVIS_REPO_SLUG
+if [ $WSTOOL = rosws ] ; then
+    $ROSWS set --git $TRAVIS_REPO_SLUG https://dummy.com/dummy -y
+fi
 ln -s $CI_SOURCE_PATH . # Link the repo we are testing to the new workspace
 cd ../
 # Install dependencies for source repos
--- travis/script.sh
+++ travis/script.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-catkin_make -j4
-catkin_make test
-catkin_make install
-rm -fr devel src build
-source install/setup.bash
-find install -iname "*.test" -print0 | xargs -0 -n1 rostest
+if [ $BUILDER = catkin ]; then
+    catkin_make -j4
+    catkin_make test
+    catkin_make install
+    rm -fr devel src build
+    source install/setup.bash
+    find install -iname "*.test" -print0 | xargs -0 -n1 rostest
+else
+    rosmake $BUILD_PACKAGES
+fi

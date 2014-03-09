6b434c2184d44c12a36fc1ad145b4fe121cf07c8
Ryohei Ueda <garaemon@gmail.com>
2014-03-10 01:27:52 +0900

    #11: add script for :install

---
+++ travis/install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+mkdir -p ~/ros/ws_$REPOSITORY_NAME/src
+cd ~/ros/ws_$REPOSITORY_NAME/src
+wstool init .
+wstool merge http://raw.github.com/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/.rosinstall
+wstool update
+ln -s $CI_SOURCE_PATH . # Link the repo we are testing to the new workspace
+cd ../
+# Install dependencies for source repos
+rosdep install -r -n --from-paths src --ignore-src --rosdistro $ROS_DISTRO -y

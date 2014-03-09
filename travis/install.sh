b826d53db469f4cc0db84a4b8785695e11c4dc3a
Ryohei Ueda <garaemon@gmail.com>
2014-03-10 01:31:41 +0900

    #11: use WSTOOL environmental variable

--- travis/install.sh
+++ travis/install.sh
@@ -2,9 +2,9 @@
 
 mkdir -p ~/ros/ws_$REPOSITORY_NAME/src
 cd ~/ros/ws_$REPOSITORY_NAME/src
-wstool init .
-wstool merge http://raw.github.com/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/.rosinstall
-wstool update
+$WSTOOL init .
+$WSTOOL merge http://raw.github.com/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/.rosinstall
+$WSTOOL update
 ln -s $CI_SOURCE_PATH . # Link the repo we are testing to the new workspace
 cd ../
 # Install dependencies for source repos

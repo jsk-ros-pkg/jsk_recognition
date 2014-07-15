65a4a8d17e104328062d0e23489da71be84bfbc6
eisoku9618 <kuroiwa@jsk.imi.i.u-tokyo.ac.jp>
2014-07-15 18:26:05 +0900

    publish u, v, true_depth and observed_depth

--- jsk_pcl_ros/src/depth_image_error_nodelet.cpp
+++ jsk_pcl_ros/src/depth_image_error_nodelet.cpp
@@ -61,6 +61,16 @@ namespace jsk_pcl_ros
     double depth_from_depth_sensor = cv_depth_image.at<float>((int)uv_point->point.y, (int)uv_point->point.x);
     NODELET_INFO("(u, v) = (%d, %d)", (int)uv_point->point.x, (int)uv_point->point.y);
     NODELET_INFO("(z, d) = (%f, %f)", uv_point->point.z, depth_from_depth_sensor);
+    if (! isnan(depth_from_depth_sensor)) {
+      jsk_pcl_ros::DepthErrorResult result;
+      result.header.frame_id = depth_image->header.frame_id;
+      result.header.stamp = depth_image->header.stamp;
+      result.u = (int)uv_point->point.x;
+      result.v = (int)uv_point->point.y;
+      result.true_depth = uv_point->point.z;
+      result.observed_depth = depth_from_depth_sensor;
+      depth_error_publisher_.publish(result);
+    }
   }
 }
 

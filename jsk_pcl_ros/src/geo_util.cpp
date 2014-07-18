29c02c2259772bad3ae94944820ff6816e1ba2df
Ryohei Ueda <ueda@jsk.t.u-tokyo.ac.jp>
2014-07-18 16:17:23 +0900

    fix normalization of the normal when creating Polygon object

--- jsk_pcl_ros/src/geo_util.cpp
+++ jsk_pcl_ros/src/geo_util.cpp
@@ -34,6 +34,7 @@
  *********************************************************************/
 
 #include "jsk_pcl_ros/geo_util.h"
+#include "jsk_pcl_ros/pcl_conversion_util.h"
 #include <algorithm>
 #include <iterator>
 #include <cfloat>
@@ -201,8 +202,8 @@ namespace jsk_pcl_ros
     n[3] = d_;
     Eigen::Matrix4d m = transform.matrix();
     Eigen::Vector4d n_d = m.transpose() * n;
-    Eigen::Vector4d n_dd = n_d.normalized();
-    
+    //Eigen::Vector4d n_dd = n_d.normalized();
+    Eigen::Vector4d n_dd = n_d / sqrt(n_d[0] * n_d[0] + n_d[1] * n_d[1] + n_d[2] * n_d[2]);
     return Plane(Eigen::Vector3d(n_dd[0], n_dd[1], n_dd[2]), n_dd[3]);
   }
   
@@ -319,6 +320,17 @@ namespace jsk_pcl_ros
       ret = ret + vertices_[i];
     }
     return ret / vertices_.size();
+  }
+
+  ConvexPolygon ConvexPolygon::fromROSMsg(const geometry_msgs::Polygon& polygon)
+  {
+    std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d> > vertices;
+    for (size_t i = 0; i < polygon.points.size(); i++) {
+      Eigen::Vector3d p;
+      pcl_conversions::fromMSGToEigen(polygon.points[i], p);
+      vertices.push_back(p);
+    }
+    return ConvexPolygon(vertices);
   }
   
 }

ab2e8d368b751804d028f9d1e0a9c61ca47c4e4b
aginika <inagaki@inagaki.mail>
2014-08-23 00:41:00 +0900

    modify with advice

--- checkerboard_detector/src/objectdetection_tf_publisher.py
+++ checkerboard_detector/src/objectdetection_tf_publisher.py
@@ -4,24 +4,16 @@ import roslib
 roslib.load_manifest("posedetection_msgs")
 roslib.load_manifest("dynamic_tf_publisher")
 from posedetection_msgs.msg import ObjectDetection
-from posedetection_msgs.msg import Object6DPose
 from geometry_msgs.msg import Transform
 from dynamic_tf_publisher.srv import *
-import tf
-import numpy
 
 class ObjectDetectionTfPublisher():
     def __init__(self):
         self.subscriber = rospy.Subscriber("ObjectDetection", ObjectDetection, self.callback);
         self.frame_id = rospy.get_param("~frame_id", "object")
-        self.latest_publish = rospy.get_param("~latest_publish", False)
-        if self.latest_publish:
-            rospy.logerr("latest publish")
         self.init_object_messages()
 
     def init_object_messages(self):
-        self.object_messages = None
-
         if rospy.has_param('~checker_board_params/position_x'):
             position_x = rospy.get_param('~checker_board_params/position_x', 0)
             position_y = rospy.get_param('~checker_board_params/position_y', 0)
@@ -30,60 +22,57 @@ class ObjectDetectionTfPublisher():
             orientation_y = rospy.get_param('~checker_board_params/orientation_y', 0)
             orientation_z = rospy.get_param('~checker_board_params/orientation_z', 0)
             orientation_w = rospy.get_param('~checker_board_params/orientation_w', 0)
-            rospy.logerr("objectdetection_tf_publisher load the calibration params :")
-            rospy.logerr("    pos : %f %f %f orientation %f %f %f %f", position_x, position_y, position_z, orientation_x, orientation_y, orientation_z, orientation_w)
+            rospy.loginfo("objectdetection_tf_publisher load the calibration params :")
+            rospy.loginfo("    pos : %f %f %f orientation %f %f %f %f", position_x, position_y, position_z, orientation_x, orientation_y, orientation_z, orientation_w)
 
-            loaded_object_messages = ObjectDetection()
-            loaded_object = Object6DPose()
-            loaded_object.pose.position.x = position_x
-            loaded_object.pose.position.y = position_y
-            loaded_object.pose.position.z = position_z
-            loaded_object.pose.orientation.x = orientation_x
-            loaded_object.pose.orientation.y = orientation_y
-            loaded_object.pose.orientation.z = orientation_z
-            loaded_object.pose.orientation.w = orientation_w
-
-            loaded_object_messages.objects = [loaded_object]
-            loaded_object_messages.header.frame_id = rospy.get_param('~checker_board_params/header_frame')
-            self.object_messages = loaded_object_messages
+            self.send_dynamic_tf_request(position_x, position_y, position_z,
+                                         orientation_x,orientation_y,orientation_z,orientation_w,
+                                         rospy.get_param('~checker_board_params/header_frame'),
+                                         self.frame_id
+                                         )
         else:
-            rospy.logerr("No parameters (~checker_board_params)  was found for object detection")
+            rospy.loginfo("No parameters (~checker_board_params)  was found for object detection")
 
     def callback(self, msg):
-        if  msg.objects or ( not self.latest_publish):
-            self.object_messages = msg
+        if  msg.objects:
+            for detected_object in msg.objects:
+                self.send_dynamic_tf_request(
+                    detected_object.pose.position.x,
+                    detected_object.pose.position.y,
+                    detected_object.pose.position.z,
+                    detected_object.pose.orientation.x,
+                    detected_object.pose.orientation.y,
+                    detected_object.pose.orientation.z,
+                    detected_object.pose.orientation.w,
+                    msg.header.frame_id,
+                    self.frame_id
+                    )
 
-    def run(self):
-        r = rospy.Rate(50)
-        while not rospy.is_shutdown():
-            if self.object_messages:
-                for detected_object in self.object_messages.objects:
-                    transform = Transform()
-                    transform.translation.x = detected_object.pose.position.x
-                    transform.translation.y = detected_object.pose.position.y
-                    transform.translation.z = detected_object.pose.position.z
-                    transform.rotation.x = detected_object.pose.orientation.x
-                    transform.rotation.y = detected_object.pose.orientation.y
-                    transform.rotation.z = detected_object.pose.orientation.z
-                    transform.rotation.w = detected_object.pose.orientation.w
+    def send_dynamic_tf_request(self, pos_x, pos_y, pos_z, ori_x, ori_y, ori_z, ori_w, parent_frame_id, child_frame_id):
+        transform = Transform()
+        transform.translation.x = pos_x
+        transform.translation.y = pos_y
+        transform.translation.z = pos_z
+        transform.rotation.x = ori_x
+        transform.rotation.y = ori_y
+        transform.rotation.z = ori_z
+        transform.rotation.w = ori_w
 
-                    set_tf_request = SetDynamicTFRequest()
-                    set_tf_request.freq = 10
-                    set_tf_request.cur_tf.header.stamp = rospy.get_rostime()
-                    set_tf_request.cur_tf.header.frame_id = self.object_messages.header.frame_id
-                    set_tf_request.cur_tf.child_frame_id = self.frame_id
-                    set_tf_request.cur_tf.transform = transform
+        set_tf_request = SetDynamicTFRequest()
+        set_tf_request.freq = 10
+        set_tf_request.cur_tf.header.stamp = rospy.get_rostime()
+        set_tf_request.cur_tf.header.frame_id = parent_frame_id
+        set_tf_request.cur_tf.child_frame_id = child_frame_id
+        set_tf_request.cur_tf.transform = transform
 
-                    rospy.wait_for_service('set_dynamic_tf')
-                    set_dynamic_tf = rospy.ServiceProxy('set_dynamic_tf', SetDynamicTF)
-                    try:
-                        res = set_dynamic_tf(set_tf_request)
-                    except rospy.ServiceException as exc:
-                        print("Service did not process request: " + str(exc))
-
-            r.sleep()
+        rospy.wait_for_service('set_dynamic_tf')
+        set_dynamic_tf = rospy.ServiceProxy('set_dynamic_tf', SetDynamicTF)
+        try:
+            res = set_dynamic_tf(set_tf_request)
+        except rospy.ServiceException as exc:
+            print("Service did not process request: " + str(exc))
 
 if __name__== '__main__':
     rospy.init_node('objectdetection_tf_publisher', anonymous=True)
     object_detection_tf_publisher = ObjectDetectionTfPublisher()
-    object_detection_tf_publisher.run()
+    rospy.spin()

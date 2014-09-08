8566ec18f3b5da3dc61451e3d61aea32935a3443
Ryohei Ueda <garaemon@gmail.com>
2014-09-08 23:12:40 +0900

    use colorCategory20 function to colorize pointcloud in ClusterPointIndicesDecomposer

--- jsk_pcl_ros/src/cluster_point_indices_decomposer_nodelet.cpp
+++ jsk_pcl_ros/src/cluster_point_indices_decomposer_nodelet.cpp
@@ -47,7 +47,7 @@
 
 #include "jsk_pcl_ros/geo_util.h"
 #include "jsk_pcl_ros/pcl_conversion_util.h"
-
+#include "jsk_pcl_ros/pcl_util.h"
 namespace jsk_pcl_ros
 {
   ClusterPointIndicesDecomposer::ClusterPointIndicesDecomposer() {}
@@ -57,14 +57,6 @@ namespace jsk_pcl_ros
   void ClusterPointIndicesDecomposer::onInit()
   {
     PCLNodelet::onInit();
-
-    colors_.push_back(makeColor(1.0, 0.0, 0.0, 1.0));
-    colors_.push_back(makeColor(0.0, 1.0, 0.0, 1.0));
-    colors_.push_back(makeColor(0.0, 0.0, 1.0, 1.0));
-    colors_.push_back(makeColor(1.0, 1.0, 0.0, 1.0));
-    colors_.push_back(makeColor(1.0, 0.0, 1.0, 1.0));
-    colors_.push_back(makeColor(0.0, 1.0, 1.0, 1.0));
-    colors_.push_back(makeColor(1.0, 1.0, 1.0, 1.0));
 
     pnh_.reset (new ros::NodeHandle (getPrivateNodeHandle ()));
     pc_pub_ = pnh_->advertise<sensor_msgs::PointCloud2>("debug_output", 1);
@@ -247,7 +239,7 @@ namespace jsk_pcl_ros
    size_t i,
    pcl::PointCloud<pcl::PointXYZRGB>& debug_output)
   {
-    uint32_t rgb = colorRGBAToUInt32(colors_[i % colors_.size()]);
+    uint32_t rgb = colorRGBAToUInt32(colorCategory20(i));
     for (size_t j = 0; j < segmented_cloud->points.size(); j++) {
       pcl::PointXYZRGB p;
       p.x= segmented_cloud->points[j].x;

56a1c06dac028a6527cf67682785baf964be9544
Ryohei Ueda <garaemon@gmail.com>
2014-09-26 14:52:03 +0900

    add new nodelet to align two pointcloud based on ICP algorithm

--- jsk_pcl_ros/catkin.cmake
+++ jsk_pcl_ros/catkin.cmake
@@ -216,6 +216,8 @@ jsk_pcl_nodelet(src/organized_pass_through_nodelet.cpp
   "jsk_pcl/OrganizedPassThrough" "organized_pass_through")
 jsk_pcl_nodelet(src/plane_reasoner_nodelet.cpp
   "jsk_pcl/PlaneReasoner" "plane_reasoner")
+jsk_pcl_nodelet(src/icp_registration_nodelet.cpp
+  "jsk_pcl/ICPRegistration" "icp_registration")
 
 add_library(jsk_pcl_ros SHARED ${jsk_pcl_nodelet_sources}
   src/grid_index.cpp src/grid_map.cpp src/grid_line.cpp src/geo_util.cpp

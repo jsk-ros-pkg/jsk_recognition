a819810d7e53e092e7066501067085166c16dcfe
Ryohei Ueda <garaemon@gmail.com>
2014-10-25 02:26:10 +0900

    Support quadratic function fitting in depth_error_calibration.py

--- jsk_pcl_ros/scripts/check_depth_error/depth_error_calibration.py
+++ jsk_pcl_ros/scripts/check_depth_error/depth_error_calibration.py
@@ -9,11 +9,17 @@ import time
 import threading
 import sys
 import rospy
+import argparse
+import csv
 
 xs = []
 ys = []
 lock = threading.Lock()
-
+u_min = None
+u_max = None
+v_min = None
+v_max = None
+model = None
 
 def query_yes_no(question, default=None):
     """Ask a yes/no question via raw_input() and return their answer.
@@ -47,44 +53,107 @@ def query_yes_no(question, default=None):
             sys.stdout.write("Please respond with 'yes' or 'no' "
                              "(or 'y' or 'n').\n")
 
+
+def genFeatureVector(x):
+    global model
+    if model == "linear":
+        return [x]
+    elif model == "quadratic":
+        return [x * x, x]
+    
 def callback(msg):
-    global xs, ys, classifier
+    global xs, ys, classifier, u_min, u_max, v_min, v_max
     with lock:
         x = msg.observed_depth
         y = msg.true_depth
-        print (x, y)
-        xs.append([x])
-        ys.append(y)
-        classifier.fit(xs, ys)
+        u = msg.u
+        v = msg.v
+        if u > u_min and u < u_max and v < v_max and v > v_min:
+            print (x, y)
+            xs.append(genFeatureVector(x))
+            ys.append(y)
+            classifier.fit(xs, ys)
+        else:
+            print "(%d, %d) is out of range" % (u, v)
+
+def modelEquationString(classifier):
+    global model, xs, ys
+    if model == "linear":
+        return "%fz + %f (score: %f)" % (classifier.coef_[0], classifier.intercept_,
+                                         classifier.score(xs, ys))
+    elif model == "quadratic":
+        return "%fz^2 %fz + %f (score: %f)" % (classifier.coef_[0],
+                                   classifier.coef_[1],
+                                   classifier.intercept_,
+                                   classifier.score(xs, ys))
+            
+def applyModel(x, clssifier):
+    global model
+    if model == "linear":
+        return classifier.coef_[0] * x + classifier.intercept_
+    elif model == "quadratic":
+        return classifier.coef_[0] * x * x + classifier.coef_[1] * x + classifier.intercept_
     
 def main():
-    global ax, xs, ys, classifier
+    global ax, xs, ys, classifier, u_min, u_max, v_min, v_max, model
+    # parse argument
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--csv')
+    parser.add_argument('--model', default="linear")
+    args = parser.parse_args(rospy.myargv()[1:])
+    model = args.model
+    if model not in ["linear", "quadratic"]:
+        raise Exception("Unknown Model: %s" % (model))
+    
+    if not args.csv:
+        sub = rospy.Subscriber("depth_image_error/output", 
+                               DepthErrorResult, callback)
     plt.ion()
     fig = plt.figure()
     ax = plt.axes([.12, .12, .8, .8])
-    sub = rospy.Subscriber("depth_image_error/output", DepthErrorResult, callback)
     r = rospy.Rate(10)
     classifier = linear_model.LinearRegression()
+    u_min = rospy.get_param("~u_min", 0)
+    u_max = rospy.get_param("~u_max", 4096)
+    v_min = rospy.get_param("~v_min", 0)
+    v_max = rospy.get_param("~v_max", 4096)
+    if args.csv:
+        for row in csv.reader(open(args.csv, "rb")):
+            x = float(row[0])
+            y = float(row[1])
+            xs.append(genFeatureVector(x))
+            ys.append(y)
+        classifier.fit(xs, ys)
     try:
         while not rospy.is_shutdown():
+            r.sleep()
             if len(xs) == 0:
                 continue
             with lock:
-                xmin = np.amin(xs)
-                xmax = np.amax(xs)
-                X_test = np.c_[xmin, xmax].T
+                xmin = np.amin([x[-1] for x in xs])
+                xmax = np.amax([x[-1] for x in xs])
+                #X_test = np.c_[xmin, xmax].T
+                X_test = [[xmin * xmin, xmin], [xmax * xmax, xmax]]
+                X_range = np.linspace(xmin, xmax, 100)
                 plt.cla()
                 ax.set_xlabel('Z from depth image')
                 ax.set_ylabel('Z from checker board')
-                ax.scatter(xs, ys, s=10, c='b', zorder=10, alpha=0.5)
-                ax.plot(X_test, classifier.predict(X_test), linewidth=2, color='red', alpha=0.5)
-                ax.plot(X_test, X_test, linewidth=2, color='green', alpha=0.5)
+                ax.scatter([[x[-1]] for x in xs], 
+                           ys, s=10, c='b', zorder=10, alpha=0.5)
+                ax.plot(X_range, 
+                        applyModel(X_range, classifier),
+                        linewidth=2, color='red', alpha=0.5)
+                ax.plot(X_range, X_range, linewidth=2, color='green', alpha=0.5)
                 plt.text(xmin, xmax,
-                         "%fz + %f (score: %f)" 
-                % (classifier.coef_[0], classifier.intercept_, classifier.score(xs, ys)),
-                fontsize=12)
+                         modelEquationString(classifier),
+                         fontsize=12)
                 plt.draw()
     finally:
+        if not args.csv:
+            print "Save calibration parameters to calibrate.csv"
+            with open("calibrate.csv", "w") as f:
+                for x, y in zip(xs, ys):
+                    f.write("%f,%f\n" % (x[0], y))
         if query_yes_no("Dump result into launch file?"):
             print "writing to calibration_parameter.launch"
             with open("calibration_parameter.launch", "w") as f:

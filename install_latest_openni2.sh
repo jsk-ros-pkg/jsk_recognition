edf037dbeb52b2f647520653c8489e3cafbdd432
Ryohei Ueda <garaemon@gmail.com>
2014-11-27 03:24:55 +0900

    Install openni2 first

--- install_latest_openni2.sh
+++ install_latest_openni2.sh
@@ -6,17 +6,19 @@ if [ ! -e /tmp/openni2 ]; then
     mkdir -p /tmp/openni2
     cd /tmp/openni2
     git clone https://github.com/atuleu/debian-openni2.git
-    echo "override_dh_shlibdeps:\n\tdh_shlibdeps $@ -- --ignore-missing-info" >> /tmp/openni2/debian-openni2/debian/rules
 else
     cd /tmp/openni2/debian-openni2
     git clean -xfd
     git reset --hard
     cd ..
 fi
+echo "override_dh_shlibdeps:\n\tdh_shlibdeps \$@ -- --ignore-missing-info" >> /tmp/openni2/debian-openni2/debian/rules
+
 cd debian-openni2
 git-buildpackage -uc -us --git-ignore-new
 cd ..
-sudo dpkg -i libopenni2_2.2.0.33~beta2-1~dev3_amd64.deb libopenni2-dbg_2.2.0.33~beta2-1~dev3_amd64.deb libopenni2-dev_2.2.0.33~beta2-1~dev3_amd64.deb
+sudo dpkg -i libopenni2_2.2.0.33~beta2-1~dev3_amd64.deb
+sudo dpkg -i libopenni2-dbg_2.2.0.33~beta2-1~dev3_amd64.deb libopenni2-dev_2.2.0.33~beta2-1~dev3_amd64.deb
 
 sudo sh -c 'cat <<EOF > /etc/udev/rules.d/40-libopenni2-0.rules
 SUBSYSTEM=="usb", ATTR{idProduct}=="0609", ATTR{idVendor}=="1d27", MODE:="0666", OWNER:="root", GROUP:="video"

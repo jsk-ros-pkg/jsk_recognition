ac1c146a5276219011fa0a40cc9373cbef854570
Ryohei Ueda <garaemon@gmail.com>
2014-11-27 03:23:25 +0900

    Add --ignore-missing-info option to build openni2 for nvidia pcs

--- install_latest_openni2.sh
+++ install_latest_openni2.sh
@@ -6,6 +6,7 @@ if [ ! -e /tmp/openni2 ]; then
     mkdir -p /tmp/openni2
     cd /tmp/openni2
     git clone https://github.com/atuleu/debian-openni2.git
+    echo "override_dh_shlibdeps:\n\tdh_shlibdeps $@ -- --ignore-missing-info" >> /tmp/openni2/debian-openni2/debian/rules
 else
     cd /tmp/openni2/debian-openni2
     git clean -xfd
@@ -13,7 +14,7 @@ else
     cd ..
 fi
 cd debian-openni2
-git-buildpackage -uc -us
+git-buildpackage -uc -us --git-ignore-new
 cd ..
 sudo dpkg -i libopenni2_2.2.0.33~beta2-1~dev3_amd64.deb libopenni2-dbg_2.2.0.33~beta2-1~dev3_amd64.deb libopenni2-dev_2.2.0.33~beta2-1~dev3_amd64.deb
 
@@ -23,4 +24,5 @@ EOF
 '
 sudo service udev restart
 
-echo done, Please compile openni2_camera from source code
+echo done, Please compile openni2_camera from source code by
+echo wstool set --git openni2_camera https://github.com/ros-drivers/openni2_camera.git -v ${ROS_DISTRO}-devel

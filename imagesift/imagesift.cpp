d784a01c7af07471615be9445f0fc6f5270b4c4a
Ryohei Ueda <garaemon@gmail.com>
2014-12-28 18:23:35 +0900

    [imagesift] support mask image

--- imagesift/imagesift.cpp
+++ imagesift/imagesift.cpp
@@ -1,3 +1,4 @@
+// -*- c-basic-offset: 4; indent-tabs-mode: nil; -*-
 // Copyright (C) 2008 Rosen Diankov (rdiankov@cs.cmu.edu)
 //
 // This program is free software: you can redistribute it and/or modify
@@ -18,43 +19,68 @@
 #include <posedetection_msgs/ImageFeature0D.h>
 #include <posedetection_msgs/Feature0DDetect.h>
 #include <image_transport/image_transport.h>
-
+#include <image_transport/subscriber_filter.h>
 #include <opencv/highgui.h>
 #include <opencv/cv.h>
 
 #include <boost/shared_ptr.hpp>
 #include <boost/thread/mutex.hpp>
-
 #include <map>
 #include <string>
 #include <cstdio>
 #include <vector>
 
 #include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
 #include <siftfast/siftfast.h>
+
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <message_filters/synchronizer.h>
+
 
 using namespace std;
 using namespace ros;
 
 class SiftNode
 {
+    typedef message_filters::sync_policies::ExactTime<
+        sensor_msgs::Image,
+        sensor_msgs::Image > SyncPolicy;
+
     boost::mutex _mutex;
     ros::NodeHandle _node;
     image_transport::ImageTransport _it;
     image_transport::Subscriber _subImage;
+    // for useMask
+    boost::shared_ptr<image_transport::SubscriberFilter> _subImageWithMask;
+        boost::shared_ptr<image_transport::SubscriberFilter> _subMask;
+    boost::shared_ptr<message_filters::Synchronizer<SyncPolicy> > _sync;
     ros::ServiceServer _srvDetect;
     Subscriber _subInfo;
     Publisher _pubSift;
     posedetection_msgs::ImageFeature0D sift_msg;
     bool _bInfoInitialized;
+    bool _useMask;
 public:
     ros::Time lasttime;
 
     SiftNode() : _it(_node)
-    { 
+    {
+        ros::NodeHandle pnh("~");
+        pnh.param("use_mask", _useMask, false);
+        
         _pubSift = _node.advertise<posedetection_msgs::ImageFeature0D>("ImageFeature0D",1);
         _srvDetect = _node.advertiseService("Feature0DDetect",&SiftNode::detect_cb,this);
-        _subImage = _it.subscribe("image",1,&SiftNode::image_cb,this);
+        if (!_useMask) {
+            _subImage = _it.subscribe("image",1,&SiftNode::image_cb,this);
+        }
+        else {
+            _subImageWithMask.reset(new image_transport::SubscriberFilter(_it, "image", 1));
+            _subMask.reset(new image_transport::SubscriberFilter(_it, "mask", 1));
+            _sync.reset(new message_filters::Synchronizer<SyncPolicy>(SyncPolicy(100), *_subImageWithMask, *_subMask));
+            _sync->registerCallback(boost::bind(&SiftNode::image_cb, this, _1, _2));
+        }
         _subInfo = _node.subscribe("camera_info",1,&SiftNode::info_cb,this);
         lasttime = ros::Time::now();
         _bInfoInitialized = false;
@@ -150,13 +176,30 @@ public:
         FreeKeypoints(keypts);
         DestroyAllImages();
 
-        ROS_INFO("imagesift: image: %d(size=%d), num: %d, sift time: %.3fs, total: %.3fs", imagemsg.header.seq,
+        ROS_INFO("imagesift: image: %d(size=%lu), num: %d, sift time: %.3fs, total: %.3fs", imagemsg.header.seq,
                  imagemsg.data.size(),  numkeys,
                  (float)(ros::Time::now()-siftbasetime).toSec(), (float)(ros::Time::now()-lasttime).toSec());
         lasttime = ros::Time::now();
         return true;
     }
 
+    void image_cb(const sensor_msgs::ImageConstPtr& msg_ptr,
+                  const sensor_msgs::ImageConstPtr& mask_ptr)
+    {
+        cv::Mat input = cv_bridge::toCvCopy(
+            msg_ptr, sensor_msgs::image_encodings::BGR8)->image;
+        cv::Mat mask = cv_bridge::toCvCopy(
+            mask_ptr, sensor_msgs::image_encodings::MONO8)->image;
+        cv::Mat masked_image;
+        input.copyTo(masked_image, mask);
+        sensor_msgs::Image::ConstPtr masked_image_msg = cv_bridge::CvImage(
+            msg_ptr->header,
+            sensor_msgs::image_encodings::BGR8,
+            masked_image).toImageMsg();
+        // sensor_msgs::Image::ConstPtr masked_image_msg_ptr = boost::make_shared<sensor_msgs::Image>(masked_image_msg);
+        image_cb(masked_image_msg);
+    }
+    
     void image_cb(const sensor_msgs::ImageConstPtr& msg_ptr)
     {
         if( _pubSift.getNumSubscribers()==0 ){ 

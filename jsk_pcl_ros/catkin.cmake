aa8e90cc8c2dfb2aa9c63d1456ef80e891fc107e
Ryohei Ueda <garaemon@gmail.com>
2015-01-14 02:35:56 +0900

    [jsk_pcl_ros] nodelet to add color to pointcloud from image

--- jsk_pcl_ros/catkin.cmake
+++ jsk_pcl_ros/catkin.cmake
@@ -282,6 +282,8 @@ jsk_pcl_nodelet(src/supervoxel_segmentation_nodelet.cpp
   "jsk_pcl/SupervoxelSegmentation" "supervoxel_segmentation")
 jsk_pcl_nodelet(src/plane_concatenator_nodelet.cpp
   "jsk_pcl/PlaneConcatenator" "plane_concatenator")
+jsk_pcl_nodelet(src/add_color_from_image_nodelet.cpp
+  "jsk_pcl/AddColorFromImage" "add_color_from_image")
 add_library(jsk_pcl_ros SHARED ${jsk_pcl_nodelet_sources}
   src/grid_index.cpp src/grid_map.cpp src/grid_line.cpp src/geo_util.cpp
   src/pcl_conversion_util.cpp src/pcl_util.cpp

f49432395be20883ac063ea4c939d75c8765adfd
Ryohei Ueda <garaemon@gmail.com>
2015-01-19 03:44:26 +0900

    [jsk_perception] color matching based on histogram and label information

--- jsk_perception/catkin.cmake
+++ jsk_perception/catkin.cmake
@@ -99,6 +99,7 @@ jsk_perception_nodelet(src/contour_finder.cpp "jsk_perception/ContourFinder" "co
 jsk_perception_nodelet(src/snake_segmentation.cpp "jsk_perception/SnakeSegmentation" "snake_segmentation")
 jsk_perception_nodelet(src/colorize_labels.cpp "jsk_perception/ColorizeLabels" "colorize_labels")
 jsk_perception_nodelet(src/grid_label.cpp "jsk_perception/GridLabel" "grid_label")
+jsk_perception_nodelet(src/color_histogram_label_match.cpp "jsk_perception/ColorHistogramLabelMatch" "color_histogram_label_match")
 # compiling jsk_perception library for nodelet
 add_library(${PROJECT_NAME} SHARED ${jsk_perception_nodelet_sources}
   ${CMAKE_CURRENT_BINARY_DIR}/build/patched-SLIC-Superpixels/slic.cpp)
---
+++ jsk_perception/include/jsk_perception/color_histogram_label_match.h
@@ -0,0 +1,89 @@
+// -*- mode: c++ -*-
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2015, JSK Lab
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/o2r other materials provided
+ *     with the distribution.
+ *   * Neither the name of the JSK Lab nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+
+#ifndef JSK_PERCEPTION_COLOR_HISTOGRAM_LABEL_MATCH_H_
+#define JSK_PERCEPTION_COLOR_HISTOGRAM_LABEL_MATCH_H_
+
+#include <jsk_topic_tools/diagnostic_nodelet.h>
+#include <sensor_msgs/Image.h>
+#include <jsk_pcl_ros/ColorHistogram.h>
+
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/exact_time.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
+#include <opencv2/opencv.hpp>
+
+namespace jsk_perception
+{
+  class ColorHistogramLabelMatch: public jsk_topic_tools::DiagnosticNodelet
+  {
+  public:
+    typedef message_filters::sync_policies::ExactTime<
+    sensor_msgs::Image,
+    sensor_msgs::Image> SyncPolicy;
+
+    ColorHistogramLabelMatch(): DiagnosticNodelet("ColorHistogramLabelMatch") {}
+  protected:
+    virtual void onInit();
+    virtual void subscribe();
+    virtual void unsubscribe();
+    virtual void match(
+      const sensor_msgs::Image::ConstPtr& image_msg,
+      const sensor_msgs::Image::ConstPtr& label_msg);
+    virtual void histogramCallback(
+      const jsk_pcl_ros::ColorHistogram::ConstPtr& histogram_msg);
+    virtual void getLabels(const cv::Mat& label, std::vector<int>& labels);
+    virtual void getMaskImage(const cv::Mat& label_image,
+                                 const int label,
+                                 cv::Mat& mask);
+    virtual double coefficients(const cv::Mat& ref_hist,
+                                const cv::Mat& target_hist);
+    virtual void normalizeHistogram(cv::Mat& hist);
+    boost::mutex mutex_;
+    boost::shared_ptr<message_filters::Synchronizer<SyncPolicy> > sync_;
+    message_filters::Subscriber<sensor_msgs::Image> sub_image_;
+    message_filters::Subscriber<sensor_msgs::Image> sub_label_;
+    ros::Subscriber sub_histogram_;
+    cv::Mat histogram_;
+    ros::Publisher pub_debug_;
+  private:
+    
+  };
+}
+
+#endif

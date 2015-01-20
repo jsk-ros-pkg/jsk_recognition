50026450d27f5c55e72be00cb85496b21a94c577
Ryohei Ueda <garaemon@gmail.com>
2015-01-20 23:48:22 +0900

    [jsk_perception] Add BlobDetector

--- jsk_perception/catkin.cmake
+++ jsk_perception/catkin.cmake
@@ -23,6 +23,7 @@ endif()
 
 # Dynamic reconfigure support
 generate_dynamic_reconfigure_options(
+  cfg/BlobDetector.cfg
   cfg/SingleChannelHistogram.cfg
   cfg/ColorHistogramLabelMatch.cfg
   cfg/GridLabel.cfg
@@ -106,6 +107,7 @@ jsk_perception_nodelet(src/grid_label.cpp "jsk_perception/GridLabel" "grid_label
 jsk_perception_nodelet(src/color_histogram_label_match.cpp "jsk_perception/ColorHistogramLabelMatch" "color_histogram_label_match")
 jsk_perception_nodelet(src/apply_mask_image.cpp "jsk_perception/ApplyMaskImage" "apply_mask_image")
 jsk_perception_nodelet(src/single_channel_histogram.cpp "jsk_perception/SingleChannelHistogram" "single_channel_histogram")
+jsk_perception_nodelet(src/blob_detector.cpp "jsk_perception/BlobDetector" "blob_detector")
 # compiling jsk_perception library for nodelet
 add_library(${PROJECT_NAME} SHARED ${jsk_perception_nodelet_sources}
   ${CMAKE_CURRENT_BINARY_DIR}/build/patched-SLIC-Superpixels/slic.cpp)

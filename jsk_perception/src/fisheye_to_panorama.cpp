ef7cf074951f9b0ecc25b16d020c96fd2dc899a9
aginika <yuto.inagaki@gmail.com>
2015-04-17 23:37:16 +0900

    [jsk_perception] Add fisheye rectify

--- jsk_perception/src/fisheye_to_panorama.cpp
+++ jsk_perception/src/fisheye_to_panorama.cpp
@@ -63,85 +63,125 @@ namespace jsk_perception
     sub_image_.shutdown();
   }
 
+
   void FisheyeToPanorama::rectify(const sensor_msgs::Image::ConstPtr& image_msg)
   {
-    
+    float max_degree = 60;
+    float max_radian = max_degree * 3.14159265 /180.0;
+    float tan_max_radian = tan(max_radian);
+    const float K = 341.656050955;
+    float max_radius = max_radian * K;
     cv::Mat distorted = cv_bridge::toCvCopy(image_msg, sensor_msgs::image_encodings::BGR8)->image;
     int l = distorted.rows / 2;
-    cv::Mat undistorted(l, l*4, CV_8UC3);
-    cv::Mat undistorted_bilinear(l, l*4, CV_8UC3);
 
-    for(int i = 0; i < undistorted.rows; ++i){
-      for(int j = 0; j < undistorted.cols; ++j){
+    if(false){
+      cv::Mat undistorted(l, l*4, CV_8UC3);
+      cv::Mat undistorted_bilinear(l, l*4, CV_8UC3);
+      for(int i = 0; i < undistorted.rows; ++i){
+        for(int j = 0; j < undistorted.cols; ++j){
 	
-	double radius = l - i;
-	double theta = 2.0 * PI * (double)(-j) / (double)(4.0 * l);
-	double fTrueX = radius * cos(theta);
-	double fTrueY = radius * sin(theta);
+          double radius = l - i;
+          double theta = 2.0 * PI * (double)(-j) / (double)(4.0 * l);
+          double fTrueX = radius * cos(theta);
+          double fTrueY = radius * sin(theta);
 
-	int x = (int)round(fTrueX) + l;
-	int y = l - (int)round(fTrueY);
-	if (x >= 0 && x < (2 * l) && y >= 0 && y < (2 * l))
-	  {
-	    for(int c = 0; c < undistorted.channels(); ++c)
-	      undistorted.data[ i * undistorted.step + j * undistorted.elemSize() + c ]
-		= distorted.data[x * distorted.step + y * distorted.elemSize() + c];
-	  }
+          int x = (int)round(fTrueX) + l;
+          int y = l - (int)round(fTrueY);
+          if (x >= 0 && x < (2 * l) && y >= 0 && y < (2 * l))
+            {
+              for(int c = 0; c < undistorted.channels(); ++c)
+                undistorted.data[ i * undistorted.step + j * undistorted.elemSize() + c ]
+                  = distorted.data[x * distorted.step + y * distorted.elemSize() + c];
+            }
 
-	fTrueX = fTrueX + (double)l;
-	fTrueY = (double)l - fTrueY;
+          fTrueX = fTrueX + (double)l;
+          fTrueY = (double)l - fTrueY;
 
-	int iFloorX = (int)floor(fTrueX);
-	int iFloorY = (int)floor(fTrueY);
-	int iCeilingX = (int)ceil(fTrueX);
-	int iCeilingY = (int)ceil(fTrueY);
+          int iFloorX = (int)floor(fTrueX);
+          int iFloorY = (int)floor(fTrueY);
+          int iCeilingX = (int)ceil(fTrueX);
+          int iCeilingY = (int)ceil(fTrueY);
 
-	if (iFloorX < 0 || iCeilingX < 0 ||
-	    iFloorX >= (2 * l) || iCeilingX >= (2 * l) ||
-	    iFloorY < 0 || iCeilingY < 0 ||
-	    iFloorY >= (2 * l) || iCeilingY >= (2 * l)) continue;
+          if (iFloorX < 0 || iCeilingX < 0 ||
+              iFloorX >= (2 * l) || iCeilingX >= (2 * l) ||
+              iFloorY < 0 || iCeilingY < 0 ||
+              iFloorY >= (2 * l) || iCeilingY >= (2 * l)) continue;
 
-	double fDeltaX = fTrueX - (double)iFloorX;
-	double fDeltaY = fTrueY - (double)iFloorY;
+          double fDeltaX = fTrueX - (double)iFloorX;
+          double fDeltaY = fTrueY - (double)iFloorY;
 
-	cv::Mat clrTopLeft(1,1, CV_8UC3), clrTopRight(1,1, CV_8UC3), clrBottomLeft(1,1, CV_8UC3), clrBottomRight(1,1, CV_8UC3);
-	for(int c = 0; c < undistorted.channels(); ++c){
-	  clrTopLeft.data[ c ] = distorted.data[iFloorX * distorted.step + iFloorY * distorted.elemSize() + c];
-	  clrTopRight.data[ c ] = distorted.data[iCeilingX * distorted.step + iFloorY * distorted.elemSize() + c];
-	  clrBottomLeft.data[ c ] = distorted.data[iFloorX * distorted.step + iCeilingY * distorted.elemSize() + c];
-	  clrBottomRight.data[ c ] = distorted.data[iCeilingX * distorted.step + iCeilingY * distorted.elemSize() + c];
-	}
+          cv::Mat clrTopLeft(1,1, CV_8UC3), clrTopRight(1,1, CV_8UC3), clrBottomLeft(1,1, CV_8UC3), clrBottomRight(1,1, CV_8UC3);
+          for(int c = 0; c < undistorted.channels(); ++c){
+            clrTopLeft.data[ c ] = distorted.data[iFloorX * distorted.step + iFloorY * distorted.elemSize() + c];
+            clrTopRight.data[ c ] = distorted.data[iCeilingX * distorted.step + iFloorY * distorted.elemSize() + c];
+            clrBottomLeft.data[ c ] = distorted.data[iFloorX * distorted.step + iCeilingY * distorted.elemSize() + c];
+            clrBottomRight.data[ c ] = distorted.data[iCeilingX * distorted.step + iCeilingY * distorted.elemSize() + c];
+          }
 
-	double fTop0 = interpolate(fDeltaX, clrTopLeft.data[0], clrTopRight.data[0]);
-	double fTop1 = interpolate(fDeltaX, clrTopLeft.data[1], clrTopRight.data[1]);
-	double fTop2 = interpolate(fDeltaX, clrTopLeft.data[2], clrTopRight.data[2]);
-	double fBottom0 = interpolate(fDeltaX, clrBottomLeft.data[0], clrBottomRight.data[0]);
-	double fBottom1 = interpolate(fDeltaX, clrBottomLeft.data[1], clrBottomRight.data[1]);
-	double fBottom2 = interpolate(fDeltaX, clrBottomLeft.data[2], clrBottomRight.data[2]);
+          double fTop0 = interpolate(fDeltaX, clrTopLeft.data[0], clrTopRight.data[0]);
+          double fTop1 = interpolate(fDeltaX, clrTopLeft.data[1], clrTopRight.data[1]);
+          double fTop2 = interpolate(fDeltaX, clrTopLeft.data[2], clrTopRight.data[2]);
+          double fBottom0 = interpolate(fDeltaX, clrBottomLeft.data[0], clrBottomRight.data[0]);
+          double fBottom1 = interpolate(fDeltaX, clrBottomLeft.data[1], clrBottomRight.data[1]);
+          double fBottom2 = interpolate(fDeltaX, clrBottomLeft.data[2], clrBottomRight.data[2]);
 
-	int i0 = (int)round(interpolate(fDeltaY, fTop0, fBottom0));
-	int i1 = (int)round(interpolate(fDeltaY, fTop1, fBottom1));
-	int i2 = (int)round(interpolate(fDeltaY, fTop2, fBottom2));
+          int i0 = (int)round(interpolate(fDeltaY, fTop0, fBottom0));
+          int i1 = (int)round(interpolate(fDeltaY, fTop1, fBottom1));
+          int i2 = (int)round(interpolate(fDeltaY, fTop2, fBottom2));
 
-	i0 = std::min(255, std::max(i0, 0));
-	i1 = std::min(255, std::max(i1, 0));
-	i2 = std::min(255, std::max(i2, 0));
-	  
-	undistorted_bilinear.data[ i * undistorted_bilinear.step + j * undistorted_bilinear.elemSize() + 0] = i0;
-	undistorted_bilinear.data[ i * undistorted_bilinear.step + j * undistorted_bilinear.elemSize() + 1] = i1;
-	undistorted_bilinear.data[ i * undistorted_bilinear.step + j * undistorted_bilinear.elemSize() + 2] = i2;
+          i0 = std::min(255, std::max(i0, 0));
+          i1 = std::min(255, std::max(i1, 0));
+          i2 = std::min(255, std::max(i2, 0));
+
+          undistorted_bilinear.data[ i * undistorted_bilinear.step + j * undistorted_bilinear.elemSize() + 0] = i0;
+          undistorted_bilinear.data[ i * undistorted_bilinear.step + j * undistorted_bilinear.elemSize() + 1] = i1;
+          undistorted_bilinear.data[ i * undistorted_bilinear.step + j * undistorted_bilinear.elemSize() + 2] = i2;
+        }
       }
-    }
 
-    pub_undistorted_image_.publish(
-				   cv_bridge::CvImage(
-						      image_msg->header,
-						      image_msg->encoding,
-						      undistorted).toImageMsg());
-    pub_undistorted_bilinear_image_.publish(cv_bridge::CvImage(
-							       image_msg->header,
-							       image_msg->encoding,
-							       undistorted_bilinear).toImageMsg());
+      pub_undistorted_image_.publish(
+                                     cv_bridge::CvImage(
+                                                        image_msg->header,
+                                                        image_msg->encoding,
+                                                        undistorted).toImageMsg());
+      pub_undistorted_bilinear_image_.publish(cv_bridge::CvImage(
+                                                                 image_msg->header,
+                                                                 image_msg->encoding,
+                                                                 undistorted_bilinear).toImageMsg());
+    }else{
+      cv::Mat undistorted(int(l * tan_max_radian * 2), int(l * tan_max_radian * 2), CV_8UC3);
+      int center_x = distorted.rows/2, center_y = distorted.cols/2;
+
+      for(int i = 0; i < distorted.rows; ++i){
+        for(int j = 0; j < distorted.cols; ++j){
+          float radius = sqrt((i-center_x)*(i-center_x) + (j-center_y)*(j-center_y));
+          float radian = radius/K;
+          if( radian < max_radian ){
+            float theta = radian;
+            float new_radius = distorted.rows/2 * tan(theta);
+            float multi = 0, new_x, new_y;
+            if(radius){
+              multi = new_radius / radius;
+              new_x = (i-center_x) * multi + center_x * tan_max_radian;
+              new_y = (j-center_y) * multi + center_y * tan_max_radian;
+            }else{
+              new_x = center_x * tan_max_radian;
+              new_y = center_y * tan_max_radian;
+            }
+
+            for(int c = 0; c < undistorted.channels(); ++c){
+              undistorted.data[ int(new_x) * undistorted.step + int(new_y) * undistorted.elemSize() + c ]
+                = distorted.data[ i * distorted.step + j * distorted.elemSize() + c];
+            }
+          }
+        }
+      }
+      pub_undistorted_image_.publish(
+                                     cv_bridge::CvImage(
+                                                        image_msg->header,
+                                                        image_msg->encoding,
+                                                        undistorted).toImageMsg());
+    }
   }
 }
 

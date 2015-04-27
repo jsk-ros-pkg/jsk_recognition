bcc2f39e2e0ae1483cf52de18214a706a2e845e0
iKrishneel <krishneel.chand.chaudhary@gmail.com>
2015-04-27 14:13:14 +0900

    Removed opencv non-free header directive

--- jsk_perception/include/jsk_perception/histogram_of_oriented_gradients.h
+++ jsk_perception/include/jsk_perception/histogram_of_oriented_gradients.h
@@ -6,11 +6,6 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/features2d/features2d.hpp>
-#include <opencv2/nonfree/features2d.hpp>
-#include <opencv2/legacy/legacy.hpp>
-#include <opencv2/ml/ml.hpp>
-#include <opencv2/nonfree/nonfree.hpp>
 
 #include <string>
 #include <vector>

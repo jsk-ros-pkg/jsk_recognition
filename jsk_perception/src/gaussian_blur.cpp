b52e47799ed92ebc302b4bb3ecd7fb82b580fb9e
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-05-19 03:49:12 +0900

    [jsk_percerption] Add gaussian_blur

---
+++ jsk_perception/src/gaussian_blur.cpp
@@ -0,0 +1,101 @@
+// -*- mode: c++ -*-
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2015, JSK Lab
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/o2r other materials provided
+ *     with the distribution.
+ *   * Neither the name of the JSK Lab nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+
+#include "jsk_perception/gaussian_blur.h"
+#include <sensor_msgs/image_encodings.h>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/opencv.hpp>
+
+namespace jsk_perception
+{
+  void GaussianBlur::onInit()
+  {
+    DiagnosticNodelet::onInit();
+    srv_ = boost::make_shared <dynamic_reconfigure::Server<Config> > (*pnh_);
+    dynamic_reconfigure::Server<Config>::CallbackType f =
+      boost::bind (&GaussianBlur::configCallback, this, _1, _2);
+    srv_->setCallback (f);
+
+    pub_ = advertise<sensor_msgs::Image>(*pnh_, "output", 1);
+  }
+
+  void GaussianBlur::subscribe()
+  {
+    sub_ = pnh_->subscribe("input", 1, &GaussianBlur::apply, this);
+  }
+
+  void GaussianBlur::unsubscribe()
+  {
+    sub_.shutdown();
+  }
+
+  void GaussianBlur::configCallback(
+    Config &config, uint32_t level)
+  {
+    boost::mutex::scoped_lock lock(mutex_);
+    kernel_size_ = config.kernel_size;
+    sigma_x_ = config.sigma_x;
+    sigma_y_ = config.sigma_y;
+  }
+
+  void GaussianBlur::apply(
+    const sensor_msgs::Image::ConstPtr& image_msg)
+  {
+    if ((image_msg->width == 0) && (image_msg->height == 0)) {
+        JSK_ROS_WARN("invalid image input");
+        return;
+    }
+    cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(
+      image_msg, image_msg->encoding);
+    cv::Mat image = cv_ptr->image;
+    if (image_msg->encoding == sensor_msgs::image_encodings::RGB8) {
+      cv::cvtColor(image, image, CV_RGB2BGR);
+    }
+    cv::Mat applied_image;
+    if (kernel_size_ % 2 == 1) {
+      cv::GaussianBlur(image, applied_image, cv::Size(kernel_size_, kernel_size_), sigma_x_, sigma_y_);
+    } else {
+      cv::GaussianBlur(image, applied_image, cv::Size(kernel_size_+1, kernel_size_+1), sigma_x_, sigma_y_);
+    }
+    pub_.publish(cv_bridge::CvImage(
+                     image_msg->header,
+                     image_msg->encoding,
+                     applied_image).toImageMsg());
+  }
+}
+
+#include <pluginlib/class_list_macros.h>
+PLUGINLIB_EXPORT_CLASS (jsk_perception::GaussianBlur, nodelet::Nodelet);

15d425bfd2c5ae3f1386e4075743bfa761d5ce18
Ryohei Ueda <garaemon@gmail.com>
2015-08-12 13:40:15 +0900

    [jsk_pcl_ros/PoseWithCovarianceStampedtoGussianPointCloud] Add offset to z-height

--- jsk_pcl_ros/src/pose_with_covariance_stamped_to_gaussian_pointcloud_nodelet.cpp
+++ jsk_pcl_ros/src/pose_with_covariance_stamped_to_gaussian_pointcloud_nodelet.cpp
@@ -77,17 +77,17 @@ namespace jsk_pcl_ros
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
     Eigen::Vector2f mean;
     Eigen::Matrix2f S;
-    if (cut_plane_ == "xy") {
+    if (cut_plane_ == "xy" || cut_plane_ == "flipped_xy") {
       mean = Eigen::Vector2f(msg->pose.pose.position.x, msg->pose.pose.position.y);
       S << msg->pose.covariance[0], msg->pose.covariance[1], 
         msg->pose.covariance[6], msg->pose.covariance[7];
     }
-    else if (cut_plane_ == "yz") {
+    else if (cut_plane_ == "yz" || cut_plane_ == "flipped_yz") {
       mean = Eigen::Vector2f(msg->pose.pose.position.y, msg->pose.pose.position.z);
       S << msg->pose.covariance[7], msg->pose.covariance[8], 
         msg->pose.covariance[13], msg->pose.covariance[14];
     }
-    else if (cut_plane_ == "zx") {
+    else if (cut_plane_ == "zx" || cut_plane_ == "flipped_zx") {
       mean = Eigen::Vector2f(msg->pose.pose.position.z, msg->pose.pose.position.x);
       S << msg->pose.covariance[14], msg->pose.covariance[12], 
         msg->pose.covariance[0], msg->pose.covariance[2];
@@ -111,17 +111,32 @@ namespace jsk_pcl_ros
         if (cut_plane_ == "xy") {
           p.x = input[0];
           p.y = input[1];
-          p.z = z;
+          p.z = z + msg->pose.pose.position.z;
         }
         else if (cut_plane_ == "yz") {
           p.y = input[0];
           p.z = input[1];
-          p.x = z;
+          p.x = z + msg->pose.pose.position.x;
         }
         else if (cut_plane_ == "zx") {
           p.z = input[0];
           p.x = input[1];
-          p.y = z;
+          p.y = z + msg->pose.pose.position.y;
+        }
+        else if (cut_plane_ == "flipped_xy") {
+          p.x = input[0];
+          p.y = input[1];
+          p.z = - (z + msg->pose.pose.position.z);
+        }
+        else if (cut_plane_ == "flipped_yz") {
+          p.y = input[0];
+          p.z = input[1];
+          p.x = - (z + msg->pose.pose.position.x);
+        }
+        else if (cut_plane_ == "flipped_zx") {
+          p.z = input[0];
+          p.x = input[1];
+          p.y = - (z + msg->pose.pose.position.y);
         }
         cloud->points.push_back(p);
       }

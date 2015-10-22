84ad1f30227acf4a81e1ed69604f35fde5a6b6d8
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-10-23 01:36:28 +0900

    [jsk_recognition_utils] Test tf::Transformer::lookupTransformation

--- jsk_recognition_utils/test/echo_tf_lookup_transform.cpp
+++ jsk_recognition_utils/test/echo_tf_lookup_transform.cpp
@@ -4,27 +4,20 @@
 
 
 void printTF(tf::StampedTransform& transform) {
-  std::cout << ">>>>>>>>>>>>>>>>>>>>>> printTF >>>>>>>>>>>>>>>>>>>>>>>>" << std::endl;
-  std::cout << "frame_id: " << transform.frame_id_ << std::endl;
-  std::cout << "child_frame_id: " << transform.child_frame_id_ << std::endl;
-  std::cout << "position_x: " << transform.getOrigin().getX() << std::endl;
-  std::cout << "position_y: " << transform.getOrigin().getY() << std::endl;
-  std::cout << "position_z: " << transform.getOrigin().getZ() << std::endl;
-  std::cout << "rotation_x: " << transform.getRotation().getX() << std::endl;
-  std::cout << "rotation_y: " << transform.getRotation().getY() << std::endl;
-  std::cout << "rotation_z: " << transform.getRotation().getZ() << std::endl;
-  std::cout << "rotation_w: " << transform.getRotation().getW() << std::endl;
-  std::cout << "<<<<<<<<<<<<<<<<<<<<<< printTF <<<<<<<<<<<<<<<<<<<<<<<<" << std::endl;
+  std::cout << "frame_id: " << transform.frame_id_ << ", "
+            << "child_frame_id: " << transform.child_frame_id_ << ", "
+            << "pos: " << transform.getOrigin().getX() << " " << transform.getOrigin().getY() << " " << transform.getOrigin().getZ() << ", "
+            << "rot: " << transform.getRotation().getX() << " " << transform.getRotation().getY() << " " << transform.getRotation().getZ() << " " << transform.getRotation().getW();
 }
 
 
 int main(int argc, char* argv[]) {
   if (argc < 3) {
-    std::cout << "Usage: simple_lookup_transform source_frame target_frame" << std::endl;
+    std::cout << "Usage: echo_tf_lookup_transform source_frame target_frame" << std::endl;
     return 1;
   }
 
-  ros::init(argc, argv, "simple_lookup_transform");
+  ros::init(argc, argv, "echo_tf_lookup_transform");
 
   boost::mutex mutex;
   tf::TransformListener* tf_listener;
@@ -39,8 +32,7 @@ int main(int argc, char* argv[]) {
   tf_listener->waitForTransform(target_frame, source_frame, ros::Time(), ros::Duration(1.0));
 
   tf::StampedTransform transform;
-  printf("lookupTransform(target_frame=\"%s\", source_frame=\"%s\"): \n",
-         target_frame.c_str(), source_frame.c_str());
+  std::cerr << "lookupTransform(target_frame=" << target_frame  << ", source_frame=" << source_frame << ")" << std::endl;
   tf_listener->lookupTransform(
     /*target_frame=*/target_frame,
     /*source_frame=*/source_frame,

34eb266858fbaf63c0089cfdfb8b4a9031758cab
MasakiMurooka <murooka@jsk.imi.i.u-tokyo.ac.jp>
2015-10-23 22:10:20 +0900

    add samples for octomap_server_contact

--- jsk_pcl_ros/scripts/test_contact_sensor.py
+++ jsk_pcl_ros/scripts/test_contact_sensor.py
@@ -7,7 +7,7 @@ from geometry_msgs.msg import Pose
 
 if __name__ == '__main__':
     rospy.init_node('test_contact_sensor')
-    contact_sensor_array_pub = rospy.Publisher('/contact_sensors_in', ContactSensorArray, latch=True)
+    contact_sensor_array_pub = rospy.Publisher('contact_sensors_in', ContactSensorArray, latch=True)
     r = rospy.Rate(10)
     while not rospy.is_shutdown():
         msg = ContactSensorArray()

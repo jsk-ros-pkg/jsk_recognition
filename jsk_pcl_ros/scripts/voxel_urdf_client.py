751e32e89152ca63117cac7eb36cfe904e9b1c42
MasakiMurooka <murooka@jsk.imi.i.u-tokyo.ac.jp>
2015-10-30 01:55:41 +0900

    move main functin contents into callback function in voxel_urdf_client.py

--- jsk_pcl_ros/scripts/voxel_urdf_client.py
+++ jsk_pcl_ros/scripts/voxel_urdf_client.py
@@ -8,12 +8,30 @@ PKG='jsk_pcl_ros'
 from visualization_msgs.msg import Marker
 from jsk_pcl_ros.srv import VoxelModelGenerate
 
-def markerCallback(msg):
-    global marker_msg
-    marker_msg = msg
+def markerCallback(marker_msg):
+    global cb_flag
+    # request service to generate urdf model
+    rospy.loginfo("Request model generation.")
+    if not generate_srv(voxel=marker_msg, name="test_model", filename="/tmp/test.urdf"):
+        rospy.logwarn("Request of model generation failed.")
+        return
+    if args.gazebo:
+        # generate model name for gazebo
+        from gazebo_msgs.srv import GetWorldProperties
+        gazebo_prop_srv = rospy.ServiceProxy("/gazebo/get_world_properties", GetWorldProperties)
+        gazebo_prop = gazebo_prop_srv()
+        model_name = args.model or "test0"
+        model_id = 0
+        while model_name in gazebo_prop.model_names:
+        model_id += 1
+        model_name = "test%d" % model_id
+        # spawn model to gazebo
+        rospy.loginfo("Spawn urdf model into gazebo. model name is %s." % model_name)
+        os.system("gzfactory spawn -f /tmp/test.urdf -m %s -x 0 -y 0 -z 1" % model_name)
+    cb_flag = True
 
 def main():
-    global marker_msg
+    global args, marker_sub, generate_srv, cb_flag
     rospy.init_node("voxel_urdf_client")
     # parse argument
     parser = argparse.ArgumentParser()
@@ -22,33 +40,12 @@ def main():
     parser.add_argument('--loop', default=True)
     args = parser.parse_args(rospy.myargv()[1:])
 
-    marker_sub = rospy.Subscriber("input", Marker, markerCallback)
+    marker_sub = rospy.Subscriber("input", Marker, markerCallback, queue_size=1)
     generate_srv = rospy.ServiceProxy("/generate_voxel_urdf", VoxelModelGenerate)
 
-    while args.loop:
+    cb_flag = False
+    while args.loop or (not cb_flag)
         rospy.sleep(1.0)
-        marker_msg = None
-        # wait for receiving voxel-grited cloud
-        while not marker_msg:
-            rospy.sleep(1.0)
-        # request service to generate urdf model
-        rospy.loginfo("Request model generation.")
-        if not generate_srv(voxel=marker_msg, name="test_model", filename="/tmp/test.urdf"):
-            rospy.logwarn("Request of model generation failed.")
-            return
-        if args.gazebo:
-            # generate model name for gazebo
-            from gazebo_msgs.srv import GetWorldProperties
-            gazebo_prop_srv = rospy.ServiceProxy("/gazebo/get_world_properties", GetWorldProperties)
-            gazebo_prop = gazebo_prop_srv()
-            model_name = args.model or "test0"
-            model_id = 0
-            while model_name in gazebo_prop.model_names:
-                model_id += 1
-                model_name = "test%d" % model_id
-            # spawn model to gazebo
-            rospy.loginfo("Spawn urdf model into gazebo. model name is %s." % model_name)
-            os.system("gzfactory spawn -f /tmp/test.urdf -m %s -x 0 -y 0 -z 1" % model_name)
 
 if __name__ == "__main__":
     main()

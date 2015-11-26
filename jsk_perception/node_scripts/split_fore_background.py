eb261c950548ecc59f7d6945db5d4532068c7a3a
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-11-26 20:53:06 +0900

    [jsk_perception] Fix header of split_fore_background

--- jsk_perception/node_scripts/split_fore_background.py
+++ jsk_perception/node_scripts/split_fore_background.py
@@ -6,6 +6,7 @@ import numpy as np
 import cv_bridge
 from jsk_recognition_utils.depth import split_fore_background
 from jsk_topic_tools import jsk_logwarn
+from jsk_topic_tools import jsk_logerr
 from jsk_topic_tools import ConnectionBasedTransport
 import message_filters
 import rospy
@@ -26,8 +27,12 @@ class SplitForeBackground(ConnectionBasedTransport):
     def subscribe(self):
         self.sub_ = message_filters.Subscriber('~input', Image)
         self.sub_depth_ = message_filters.Subscriber('~input/depth', Image)
-        sync = message_filters.TimeSynchronizer(
-            [self.sub_, self.sub_depth_], queue_size=10)
+        if rospy.get_param('~approximate_sync', False):
+            sync = message_filters.ApproximateTimeSynchronizer(
+                [self.sub_, self.sub_depth_], queue_size=10, slop=.1)
+        else:
+            sync = message_filters.TimeSynchronizer(
+                [self.sub_, self.sub_depth_], queue_size=10)
         sync.registerCallback(self._apply)
 
     def unsubscribe(self):
@@ -42,6 +47,7 @@ class SplitForeBackground(ConnectionBasedTransport):
                         .format(depth_msg.encoding))
         if not (img_msg.height == depth_msg.height and
                 img_msg.width == depth_msg.width):
+            jsk_logerr('size of raw and depth image is different')
             return
         # split fg/bg and get each mask
         bridge = cv_bridge.CvBridge()
@@ -55,20 +61,28 @@ class SplitForeBackground(ConnectionBasedTransport):
         fg_mask, bg_mask = split_fore_background(depth)
         # publish cropped
         img = bridge.imgmsg_to_cv2(img_msg, desired_encoding='bgr8')
+        # fg
         fg = img.copy()
         fg[~fg_mask] = 0
+        fg_msg = bridge.cv2_to_imgmsg(fg, encoding=img_msg.encoding)
+        fg_msg.header = img_msg.header
+        self.fg_pub_.publish(fg_msg)
+        # bg
         bg = img.copy()
         bg[~bg_mask] = 0
-        self.fg_pub_.publish(
-            bridge.cv2_to_imgmsg(fg, encoding=img_msg.encoding))
-        self.bg_pub_.publish(
-            bridge.cv2_to_imgmsg(bg, encoding=img_msg.encoding))
+        bg_msg = bridge.cv2_to_imgmsg(bg, encoding=img_msg.encoding)
+        bg_msg.header = img_msg.header
+        self.bg_pub_.publish(bg_msg)
+        # fg_mask
         fg_mask = (fg_mask * 255).astype(np.uint8)
+        fg_mask_msg = bridge.cv2_to_imgmsg(fg_mask, encoding='mono8')
+        fg_mask_msg.header = depth_msg.header
+        self.fg_mask_pub_.publish(fg_mask_msg)
+        # bg_mask
         bg_mask = (bg_mask * 255).astype(np.uint8)
-        self.fg_mask_pub_.publish(
-            bridge.cv2_to_imgmsg(fg_mask, encoding='mono8'))
-        self.bg_mask_pub_.publish(
-            bridge.cv2_to_imgmsg(bg_mask, encoding='mono8'))
+        bg_mask_msg = bridge.cv2_to_imgmsg(bg_mask, encoding='mono8')
+        bg_mask_msg.header = depth_msg.header
+        self.bg_mask_pub_.publish(bg_mask_msg)
 
 
 if __name__ == '__main__':

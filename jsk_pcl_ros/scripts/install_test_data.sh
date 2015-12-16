68858a64a4b2dc91e3a91205959ce073374bbf12
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-12-16 22:45:25 +0900

    [jsk_pcl_ros] Download test_data with quiet mode

--- jsk_pcl_ros/scripts/install_test_data.sh
+++ jsk_pcl_ros/scripts/install_test_data.sh
@@ -7,7 +7,7 @@ cd $TEST_DATA_DIR
 
 log_file=2015-11-04-19-37-29_baxter-kiva-object-in-hand-cloud.tgz
 if [ ! -f $log_file ]; then
-  gdown "https://drive.google.com/uc?id=0B9P1L--7Wd2vSE5XMmJQczl3NWc&export=download" -O $log_file
+  gdown "https://drive.google.com/uc?id=0B9P1L--7Wd2vSE5XMmJQczl3NWc&export=download" -O $log_file --quiet
   tar zxf 2015-11-04-19-37-29_baxter-kiva-object-in-hand-cloud.tgz
   rosbag decompress 2015-11-04-19-37-29_baxter-kiva-object-in-hand-cloud/vision.compressed.bag
 fi

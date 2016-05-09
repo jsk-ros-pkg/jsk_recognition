e6ac379cfa2f592029283576334cce127e0a0821
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-05-09 14:34:35 +0900

    Add label color utility function

--- jsk_recognition_utils/python/jsk_recognition_utils/__init__.py
+++ jsk_recognition_utils/python/jsk_recognition_utils/__init__.py
@@ -1,4 +1,5 @@
 import jsk_recognition_utils.chainermodels
+import jsk_recognition_utils.color
 from jsk_recognition_utils.conversations import *  # NOQA
 from jsk_recognition_utils.feature import *  # NOQA
 from jsk_recognition_utils.mask import *  # NOQA

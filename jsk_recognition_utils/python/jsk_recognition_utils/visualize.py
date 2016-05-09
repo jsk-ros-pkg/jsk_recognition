99770d7ab0e27cb5b97d20a9c3f252747545035d
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-05-09 15:41:58 +0900

    Remove color_gategoryXX (use labelcolormap)

--- jsk_recognition_utils/python/jsk_recognition_utils/visualize.py
+++ jsk_recognition_utils/python/jsk_recognition_utils/visualize.py
@@ -8,8 +8,6 @@ import cv2
 import numpy as np
 import PIL
 
-from std_msgs.msg import ColorRGBA
-
 
 def centerize(src, dst_shape):
     """Centerize image for specified image size
@@ -87,45 +85,6 @@ def get_tile_image(imgs, tile_shape=None, result_img=None):
         img = centerize(img, (max_height, max_width, 3))
         imgs[i] = img
     return _tile_images(imgs, tile_shape, result_img)
-
-
-def color_category20(i):
-    colors = (0x1f77b4,
-              0xaec7e8,
-              0xff7f0e,
-              0xffbb78,
-              0x2ca02c,
-              0x98df8a,
-              0xd62728,
-              0xff9896,
-              0x9467bd,
-              0xc5b0d5,
-              0x8c564b,
-              0xc49c94,
-              0xe377c2,
-              0xf7b6d2,
-              0x7f7f7f,
-              0xc7c7c7,
-              0xbcbd22,
-              0xdbdb8d,
-              0x17becf,
-              0x9edae5)
-    c = colors[i % 20]
-    return ColorRGBA(r=(c >> 16) / 255.0, g=((c >> 8) & 255) / 255.0, b=(c & 255) / 255.0, a=1.0)
-
-def color_category10(i):
-    colors = (0x1f77b4,
-              0xff7f0e,
-              0x2ca02c,
-              0xd62728,
-              0x9467bd,
-              0x8c564b,
-              0xe377c2,
-              0x7f7f7f,
-              0xbcbd22,
-              0x17becf)
-    c = colors[i % 10]
-    return ColorRGBA(r=(c >> 16) / 255.0, g=((c >> 8) & 255) / 255.0, b=(c & 255) / 255.0, a=1.0)
 
 
 def colorize_cluster_indices(image, cluster_indices, alpha=0.3, image_alpha=1):

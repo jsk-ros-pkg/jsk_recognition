2f852d6987af53540e48cd08f449f9010f3faff6
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-05-09 14:41:03 +0900

    Use labelcolormap in 'rect_array_to_image_marker.py'

--- jsk_perception/node_scripts/rect_array_to_image_marker.py
+++ jsk_perception/node_scripts/rect_array_to_image_marker.py
@@ -4,7 +4,8 @@ import rospy
 from jsk_topic_tools import ConnectionBasedTransport
 from jsk_recognition_msgs.msg import Rect, RectArray
 from image_view2.msg import ImageMarker2
-from jsk_recognition_utils import color_category10
+import jsk_recognition_utils
+from std_msgs.msg import ColorRGBA
 from geometry_msgs.msg import Point
 class RectArrayToImageMarker(ConnectionBasedTransport):
     def __init__(self):
@@ -21,16 +22,20 @@ class RectArrayToImageMarker(ConnectionBasedTransport):
         marker = ImageMarker2()
         marker.header = msg.header
         marker.type = ImageMarker2.LINE_LIST
-        for rect, rect_i in zip(msg.rects, range(len(msg.rects))):
+        n_colors = min(len(msg.rects), 256)
+        cmap = jsk_recognition_utils.color.labelcolormap(n_colors)
+        for rect, rect_i in zip(msg.rects, range(n_colors)):
             points = [(rect.x, rect.y),
                       (rect.x, rect.y + rect.height),
                       (rect.x + rect.width, rect.y + rect.height),
                       (rect.x + rect.width, rect.y)]
+            color = cmap[rect_i]
+            color_msg = ColorRGBA(r=color[0], g=color[1], b=color[2])
             for i, j in ((0, 1), (1, 2), (2, 3), (3, 0)):
                 marker.points.append(Point(x=points[i][0], y=points[i][1]))
                 marker.points.append(Point(x=points[j][0], y=points[j][1]))
-                marker.outline_colors.append(color_category10(rect_i))
-                marker.outline_colors.append(color_category10(rect_i))
+                marker.outline_colors.append(color_msg)
+                marker.outline_colors.append(color_msg)
         self.pub.publish(marker)
 
 

025489b586723524cde89b8245c52dd915e193ba
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-05-21 19:21:35 +0900

    Recognize object with VGG16 net

--- jsk_recognition_utils/python/jsk_recognition_utils/chainermodels/__init__.py
+++ jsk_recognition_utils/python/jsk_recognition_utils/chainermodels/__init__.py
@@ -1,5 +1,13 @@
+from jsk_recognition_utils.chainermodels import vgg16
+from jsk_recognition_utils.chainermodels import vgg16_batch_normalization
 from jsk_recognition_utils.chainermodels import vgg16_fast_rcnn
 from jsk_recognition_utils.chainermodels import vgg_cnn_m_1024
 
+
+# VGG16 Object Recognition Network
+VGG16 = vgg16.VGG16
+VGG16BatchNormalization = vgg16_batch_normalization.VGG16BatchNormalization
+
+# FastRCNN
 VGG16FastRCNN = vgg16_fast_rcnn.VGG16FastRCNN
 VGG_CNN_M_1024 = vgg_cnn_m_1024.VGG_CNN_M_1024

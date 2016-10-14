8f98d93ea05cc9bd18cc8ba8332d443c0931c022
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-10-15 05:06:32 +0900

    Fix year for license

--- jsk_perception/include/jsk_perception/consensus_tracking.h
+++ jsk_perception/include/jsk_perception/consensus_tracking.h
@@ -2,7 +2,7 @@
 /*********************************************************************
  * Software License Agreement (BSD License)
  *
- *  Copyright (c) 2015, JSK Lab
+ *  Copyright (c) 2016, JSK Lab
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without

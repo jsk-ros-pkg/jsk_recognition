bff8eb6ba875891843403627440fb6a89003283f
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-10-15 23:27:26 +0900

    Install sample data for consensus_tracking

--- jsk_perception/scripts/install_sample_data.py
+++ jsk_perception/scripts/install_sample_data.py
@@ -17,6 +17,17 @@ def main():
         ],
     )
 
+    download_data(
+        pkg_name=PKG,
+        path='sample/data/2016-10-15-23-21-42_moving_bottle.bag',
+        url='https://drive.google.com/uc?id=0B9P1L--7Wd2vTDZOZGhrTTNvc00',
+        md5='ad4e7d298c0d9985295d93e47c7b03e6',
+        extract=False,
+        compressed_bags=[
+            'sample/data/2016-10-15-23-21-42_sample_consensus_tracking.bag',
+        ],
+    )
+
 
 if __name__ == '__main__':
     main()

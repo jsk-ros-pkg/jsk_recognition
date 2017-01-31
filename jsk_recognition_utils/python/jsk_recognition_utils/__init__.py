8eb96c34f48e58493e27b464c12a8f8d7bf9971c
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-02-01 04:16:29 +0900

    Compute box overlap and publish it
    intersect-over-union (overlap)  = volume_tp / (volume_fn + volume_fp +
    volume_tp)

--- jsk_recognition_utils/python/jsk_recognition_utils/__init__.py
+++ jsk_recognition_utils/python/jsk_recognition_utils/__init__.py
@@ -1,6 +1,23 @@
-import jsk_recognition_utils.chainermodels
-import jsk_recognition_utils.color
-from jsk_recognition_utils.conversations import *  # NOQA
-from jsk_recognition_utils.feature import *  # NOQA
-from jsk_recognition_utils.mask import *  # NOQA
-from jsk_recognition_utils.visualize import *  # NOQA
+from jsk_recognition_utils import chainermodels
+from jsk_recognition_utils import color
+from jsk_recognition_utils import conversations
+from jsk_recognition_utils import feature
+from jsk_recognition_utils import mask
+from jsk_recognition_utils import visualize
+from jsk_recognition_utils import geometry
+
+
+bounding_box_msg_to_aabb = conversations.bounding_box_msg_to_aabb
+rects_msg_to_ndarray = conversations.rects_msg_to_ndarray
+
+BagOfFeatures = feature.BagOfFeatures
+decompose_descriptors_with_label = feature.decompose_descriptors_with_label
+
+bounding_rect_of_mask = mask.bounding_rect_of_mask
+descent_closing = mask.descent_closing
+
+centerize = visualize.centerize
+colorize_cluster_indices = visualize.colorize_cluster_indices
+get_tile_image = visualize.get_tile_image
+
+get_overlap_of_aabb = geometry.get_overlap_of_aabb

dcf41a4e285bdcee720470c459cc591f99983f44
Shingo Kitagawa <shingogo@hotmail.co.jp>
2017-05-08 14:10:57 +0900

    add alex_object_recognition node

--- jsk_recognition_utils/python/jsk_recognition_utils/chainermodels/__init__.py
+++ jsk_recognition_utils/python/jsk_recognition_utils/chainermodels/__init__.py
@@ -1,8 +1,13 @@
+from jsk_recognition_utils.chainermodels import alex
+from jsk_recognition_utils.chainermodels import alex_batch_normalization
 from jsk_recognition_utils.chainermodels import vgg16
 from jsk_recognition_utils.chainermodels import vgg16_batch_normalization
 from jsk_recognition_utils.chainermodels import vgg16_fast_rcnn
 from jsk_recognition_utils.chainermodels import vgg_cnn_m_1024
 
+# Alex Object Recognition Network
+Alex = alex.Alex
+AlexBatchNormalization = alex_batch_normalization.AlexBatchNormalization
 
 # VGG16 Object Recognition Network
 VGG16 = vgg16.VGG16

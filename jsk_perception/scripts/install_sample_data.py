fc912be72450df7cfc8cf0c3fc5908512394cd7d
iory <ab.ioryz@gmail.com>
2017-06-20 12:08:44 +0900

    [jsk_perception/people_pose_estimation_2d] Changed sample bag file

--- jsk_perception/scripts/install_sample_data.py
+++ jsk_perception/scripts/install_sample_data.py
@@ -30,10 +30,10 @@ def main():
 
     download_data(
         pkg_name=PKG,
-        path='sample/data/2017-06-16-12-00-00_people_images_in_lab.bag.tgz',
-        url='https://drive.google.com/uc?id=0B_NiLAzvehC9TTNPSGc0OUlIUDg',
-        md5='7f997176ea2ef2f16aa297de68e09d8d',
-        extract=False,
+        path='sample/data/2017-06-20-12-00-00_people_images_in_lab.bag.tgz',
+        url='https://drive.google.com/uc?id=0B_NiLAzvehC9Yzl4YzB1WnlFcXM',
+        md5='0a397a2ff14c07954cc0b9178e33600d',
+        extract=True,
     )
 
 

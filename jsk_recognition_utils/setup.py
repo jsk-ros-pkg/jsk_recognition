1ece139166a7a7b2788f92011075c4def23904a7
Shingo Kitagawa <shingogo@hotmail.co.jp>
2017-07-08 01:25:04 +0900

    install chainermodels dir in setup.py

--- jsk_recognition_utils/setup.py
+++ jsk_recognition_utils/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['jsk_recognition_utils'],
+    packages=['jsk_recognition_utils', 'jsk_recognition_utils.chainermodels'],
     package_dir={'': 'python'},
 )
 

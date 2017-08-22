983a0f7f228c372943b045203aa582b9d07e1def
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-08-22 23:26:21 +0900

    Add docker image of ros-indigo-base-jsk-recognition

--- docker/build.sh
+++ docker/build.sh
@@ -2,6 +2,10 @@
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# ros-indigo-pcl1.8
-(cd ${DIR}/ros/indigo/pcl1.8 && sudo docker build -t ros-indigo-pcl1.8 .)
-(echo -e "FROM ros-indigo-pcl1.8\nRUN apt-get update\nRUN apt-get -y upgrade\nEXPOSE 22" | sudo docker build -t ros-indigo-pcl1.8 -)
+set -x
+
+(cd ${DIR}/ros/indigo/base && sudo docker build --no-cache -t ros-indigo-base-jsk-recognition .)
+
+(cd ${DIR}/ros/indigo/pcl1.8 && sudo docker build --no-cache -t ros-indigo-pcl1.8-jsk-recognition .)
+
+set +x

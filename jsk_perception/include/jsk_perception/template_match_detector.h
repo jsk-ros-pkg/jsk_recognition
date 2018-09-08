f5c479a883a29da005817cd41f930f97bf65c723
iory <ab.ioryz@gmail.com>
2018-09-08 23:51:48 +0900

    [jsk_perception/template_matching] Year changed

--- jsk_perception/include/jsk_perception/template_match_detector.h
+++ jsk_perception/include/jsk_perception/template_match_detector.h
@@ -2,7 +2,7 @@
 /*********************************************************************
  * Software License Agreement (BSD License)
  *
- *  Copyright (c) 2017, JSK Lab
+ *  Copyright (c) 2018, JSK Lab
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without

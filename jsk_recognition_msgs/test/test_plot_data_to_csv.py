94a95428eb07a9eacfa12dbfcd996716be82405c
Yuto Uchimi <momosama1072@gmail.com>
2019-06-26 10:38:45 +0900

    Wait at most ~timeout [sec] in plot_data_to_csv.test

--- jsk_recognition_msgs/test/test_plot_data_to_csv.py
+++ jsk_recognition_msgs/test/test_plot_data_to_csv.py
@@ -10,8 +10,12 @@ import rostest
 class TestPlotDataToCsv(unittest.TestCase):
 
     def test_plot_data_to_csv(self):
-        csv_path = rospy.get_param('~csv_path')
-        self.assertTrue(osp.isfile(osp.expanduser(csv_path)))
+        csv_path = osp.expanduser(rospy.get_param('~csv_path'))
+        timeout = rospy.Duration(rospy.get_param('~timeout', 5.0))
+        start = rospy.Time.now()
+        while rospy.Time.now() - start < timeout and not osp.exists(csv_path):
+            rospy.sleep(0.1)
+        self.assertTrue(osp.isfile(csv_path))
 
 
 if __name__ == '__main__':

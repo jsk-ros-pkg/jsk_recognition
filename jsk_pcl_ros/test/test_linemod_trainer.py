e1c3146a0a81de10af872bb67a73ee8577150974
Yuto Uchimi <momosama1072@gmail.com>
2019-08-06 11:04:34 +0900

    Fix ~timeout param in test_linemod_trainer.py

--- jsk_pcl_ros/test/test_linemod_trainer.py
+++ jsk_pcl_ros/test/test_linemod_trainer.py
@@ -13,7 +13,7 @@ class TestLINEMODTrainer(unittest.TestCase):
         linemod_path = osp.expanduser(rospy.get_param('~linemod_path'))
         pcd_path = osp.expanduser(rospy.get_param('~pcd_path'))
         yaml_path = osp.expanduser(rospy.get_param('~yaml_path'))
-        timeout = rospy.Duration(rospy.get_param('timeout', 30.0))
+        timeout = rospy.Duration(rospy.get_param('~timeout', 30.0))
         rospy.sleep(2.0)  # Wait a moment until /clock is published.
         start = rospy.Time.now()
         while (rospy.Time.now() - start < timeout and

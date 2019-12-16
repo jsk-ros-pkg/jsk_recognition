afd86e4b0c7c26902464810dfafaf08515b0ea72
Naoya Yamaguchi <708yamaguchi@gmail.com>
2019-12-16 16:39:30 +0900

    fix size of spectrogram

--- jsk_perception/audio_to_spectrogram/spectrum_to_spectrogram.py
+++ jsk_perception/audio_to_spectrogram/spectrum_to_spectrogram.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import cv2
 from cv_bridge import CvBridge
 import numpy as np
 import rospy
@@ -15,42 +16,49 @@ class SpectrumToSpectrogram(object):
 
     def __init__(self):
         super(SpectrumToSpectrogram, self).__init__()
-
+        # Set spectrogram shape
+        self.image_height = rospy.get_param('~image_height', 300)
+        self.image_width = rospy.get_param('~image_width', 300)
+        # Get spectrum length
         spectrum_msg = rospy.wait_for_message('~spectrum', Spectrum)
         spectrum_len = len(spectrum_msg.amplitude)
-
-        # Spectrogram config
+        # Buffer for spectrum topic
+        self.spectrogram = np.zeros((0, spectrum_len), dtype=np.float32)
+        self.spectrum_stamp = []
         # Period[s] to store audio data to create one spectrogram topic
-        spectrogram_period = rospy.get_param('~spectrogram_period', 5)
-        # How many times fft is executed in one second
-        # fft_exec_rate equals to input spectrum hz and output spectrogram hz
-        fft_exec_rate = rospy.get_param('~fft_exec_rate', 50)
-        # self.spectrogram_len equals to horizontal pixel of output spectrogram
-        self.spectrogram_len = int(
-            fft_exec_rate * spectrogram_period)
-        self.spectrogram = np.zeros((0, spectrum_len),
-                                    dtype=np.float32)
-        rospy.logwarn('Please wait for the first spectrogram to come.')
-
-        # ROS
+        self.spectrogram_period = rospy.get_param('~spectrogram_period', 5)
+        # ROS subscriber and publisher
         rospy.Subscriber(
             '~spectrum', Spectrum, self.audio_cb)
         self.pub_spectrogram = rospy.Publisher(
             '~spectrogram', Image, queue_size=1)
-        rospy.Timer(rospy.Duration(1. / fft_exec_rate), self.timer_cb)
+        rospy.Timer(
+            rospy.Duration(
+                float(self.spectrogram_period) / self.image_width),
+            self.timer_cb)
         self.bridge = CvBridge()
 
     def audio_cb(self, msg):
-        # Create and publish spectrogram
+        # Add spectrum msg to buffer
         spectrum = np.array(msg.amplitude, dtype=np.float32)
         self.spectrogram = np.concatenate(
             [self.spectrogram, spectrum[None]])
-        self.spectrogram = self.spectrogram[-self.spectrogram_len:]
+        self.spectrum_stamp.append(msg.header.stamp)
 
     def timer_cb(self, timer):
-        if self.spectrogram.shape[0] < self.spectrogram_len:
+        if self.spectrogram.shape[0] == 0:
             return
+        # Extract spectrogram of last (self.spectrogram_period) seconds
+        time_now = rospy.Time.now()
+        for i, stamp in enumerate(self.spectrum_stamp):
+            if (time_now - stamp).to_sec() > self.spectrogram_period:
+                self.spectrum_stamp = self.spectrum_stamp[i:]
+                self.spectrogram = self.spectrogram[i:]
+        # Reshape spectrogram
         spectrogram = self.spectrogram.transpose(1, 0)[::-1, :]
+        spectrogram = cv2.resize(
+            spectrogram, (self.image_width, self.image_height))
+        # Publish spectrogram
         spectrogram_msg = self.bridge.cv2_to_imgmsg(spectrogram, '32FC1')
         spectrogram_msg.header.stamp = rospy.Time.now()
         self.pub_spectrogram.publish(spectrogram_msg)

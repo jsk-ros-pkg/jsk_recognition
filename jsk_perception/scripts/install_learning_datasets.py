f2d4c00b5e2c4cde5b969e16118a8d5e89eb2a5e
Yuto Uchimi <momosama1072@gmail.com>
2020-01-27 20:53:59 +0900

    Fix directory name of dataset extracted from tar ball

--- jsk_perception/scripts/install_learning_datasets.py
+++ jsk_perception/scripts/install_learning_datasets.py
@@ -34,8 +34,8 @@ def main():
     download_data(
         pkg_name=PKG,
         path='learning_datasets/human_size_mirror_dataset.tgz',
-        url='https://drive.google.com/uc?id=1MJAvGKr_IUoX_PK9GGwkQerxHrta84av',
-        md5='addcd2da612b506859723e6ece34ddb3',
+        url='https://drive.google.com/uc?id=1uImtm0Drv5prFBUzxHxVcRTH-CESXj7d',
+        md5='27d0b6937a749d2643f66d378dd2d6d6',
         quiet=quiet,
         extract=True,
     )

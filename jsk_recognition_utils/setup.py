b336ebdd63682344fe0b84a25694ee9b339cb514
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2020-05-26 08:37:11 +0000

    upgrade package.xml to format=3, migrate to noetic with ROS_PYTHON_VERSION=2/3, use multiple ROS distro strategy http://wiki.ros.org/noetic/Migration

--- jsk_recognition_utils/setup.py
+++ jsk_recognition_utils/setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 from setuptools import find_packages
 from catkin_pkg.python_setup import generate_distutils_setup

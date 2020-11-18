adf7c9af418288df6832d16a5b5c695280c2cd6c
MiyabiTane <miyabitane@yahoo.co.jp>
2020-11-18 12:33:11 +0900

    move pub and sub to init

--- jsk_pcl_ros_utils/scripts/xyz_to_screenpoint.py
+++ jsk_pcl_ros_utils/scripts/xyz_to_screenpoint.py
@@ -11,10 +11,18 @@ class XYZToScreenPoint(object):
     def __init__(self):
         self.cameramodels = PinholeCameraModel()
         self.is_camera_arrived = False
+        self.header = None
+
+        # publish info
+        self.pub = rospy.Publisher("~output", PointStamped, queue_size=1)
+
+        rospy.Subscriber('~input/camera_info', CameraInfo, self.camera_info_cb)
+        rospy.Subscriber('~input', PointStamped, self.point_stamped_cb)
 
     def camera_info_cb(self, msg):
         self.cameramodels.fromCameraInfo(msg)
         self.is_camera_arrived = True
+        self.header = msg.header
 
     def point_stamped_cb(self, msg):
         if not self.is_camera_arrived:
@@ -22,22 +30,18 @@ class XYZToScreenPoint(object):
         point = (msg.point.x, msg.point.y, msg.point.z)
         u, v = self.cameramodels.project3dToPixel(point)
         rospy.logdebug("u, v : {}, {}".format(u, v))
-        # publish info
-        pub = rospy.Publisher("~output", PointStamped, queue_size=1)
         pub_msg = PointStamped()
-        pub_msg.header = msg.header
+        pub_msg.header = self.header
         pub_msg.point.x = u
         pub_msg.point.y = v
         pub_msg.point.z = 0
-        pub.publish(pub_msg)
+        self.pub.publish(pub_msg)
 
-    def subscribeCameraInfo(self):
-        rospy.Subscriber('~input/camera_info', CameraInfo, self.fromCameraInfo_cb)
-        rospy.Subscriber('~input', PointStamped, self.project3dToPixel_cb)
-
+    # def subscribeCameraInfo(self):
+        
 
 if __name__ == '__main__':
     rospy.init_node("xyz_to_screenpoint")
     xyz_to_screenpoint = XYZToScreenPoint()
-    xyz_to_screenpoint.subscribeCameraInfo()
+    # xyz_to_screenpoint.subscribeCameraInfo()
     rospy.spin()

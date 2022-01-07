e8b4807c70bceb9370bbed64b414c60373db856a
iory <ab.ioryz@gmail.com>
2022-01-07 12:34:13 +0900

    [audio_to_spectogram] Use ConnectionBasedTransport and AudioBuffer

--- audio_to_spectrogram/scripts/audio_amplitude_plot.py
+++ audio_to_spectrogram/scripts/audio_amplitude_plot.py
@@ -3,38 +3,25 @@
 
 from __future__ import division
 
-import threading
-
-from audio_common_msgs.msg import AudioData
 import cv_bridge
 from dynamic_reconfigure.server import Server
+from jsk_topic_tools import ConnectionBasedTransport
 import matplotlib.pyplot as plt
 import numpy as np
 import rospy
 import sensor_msgs.msg
 
+from audio_to_spectrogram import AudioBuffer
 from audio_to_spectrogram.cfg import AudioAmplitudePlotConfig as Config
 from audio_to_spectrogram import convert_matplotlib_to_img
 
 
-class AudioAmplitudePlot(object):
+class AudioAmplitudePlot(ConnectionBasedTransport):
 
     def __init__(self):
         super(AudioAmplitudePlot, self).__init__()
 
-        self.lock = threading.Lock()
-
-        # Audio topic config
-        # The number of channels in audio data
-        self.n_channel = rospy.get_param('~n_channel', 1)
-        # Sampling rate of microphone (namely audio topic).
-        self.mic_sampling_rate = rospy.get_param('~mic_sampling_rate', 16000)
-        # Bits per one audio data
-        bitdepth = rospy.get_param('~bitdepth', 16)
-        if bitdepth == 16:
-            self.dtype = 'int16'
-        else:
-            rospy.logerr("'~bitdepth' {} is unsupported.".format(bitdepth))
+        self.audio_buffer = AudioBuffer.from_rosparam()
 
         # Set matplotlib config
         self.fig = plt.figure(figsize=(8, 5))
@@ -47,49 +34,39 @@ class AudioAmplitudePlot(object):
         # ROS dynamic reconfigure
         self.srv = Server(Config, self.config_callback)
 
-        # ROS publisher and subscriber
-        self.pub_img = rospy.Publisher(
+        self.pub_img = self.advertise(
             '~output/viz', sensor_msgs.msg.Image, queue_size=1)
-        self.sub_audio = rospy.Subscriber(
-            '~audio', AudioData, self.audio_cb)
 
-        # set time callback.
+    def start_timer(self):
         rate = rospy.get_param('~rate', 10)
         if rate == 0:
             rospy.logwarn('You cannot set 0 as the rate; change it to 10.')
             rate = 10
-        rospy.Timer(rospy.Duration(1.0 / rate), self.timer_cb)
+        self.timer = rospy.Timer(rospy.Duration(1.0 / rate), self.timer_cb)
+
+    def stop_timer(self):
+        self.timer.shutdown()
+
+    def subscribe(self):
+        self.audio_buffer.subscribe()
+        self.start_timer()
+
+    def unsubscribe(self):
+        self.audio_buffer.unsubscribe()
+        self.stop_timer()
 
     def config_callback(self, config, level):
-        with self.lock:
-            self.maximum_amplitude = config.maximum_amplitude
-            self.window_size = config.window_size
-
-            # Audio topic buffer
-            self.audio_buffer = np.array([], dtype=self.dtype)
-            # How long audio_buffer should be for one audio topic
-            self.audio_buffer_len = int(
-                self.mic_sampling_rate * self.window_size)
-
+        self.maximum_amplitude = config.maximum_amplitude
+        self.window_size = config.window_size
+        self.audio_buffer.window_size = self.window_size
         return config
 
-    def audio_cb(self, msg):
-        # Convert audio buffer to int array
-        data = msg.data
-        audio_buffer = np.frombuffer(data, dtype=self.dtype)
-        # Retreive one channel data
-        audio_buffer = audio_buffer[0::self.n_channel]
-        # Save audio msg to audio_buffer
-        with self.lock:
-            self.audio_buffer = np.append(
-                self.audio_buffer, audio_buffer)
-            self.audio_buffer = self.audio_buffer[
-                -self.audio_buffer_len:]
-
     def timer_cb(self, timer):
-        with self.lock:
-            amp = self.audio_buffer.copy()
-            times = np.linspace(-self.window_size, 0.0, len(amp))
+        window_size = self.window_size
+        amp = self.audio_buffer.read(window_size)
+        if len(amp) == 0:
+            return
+        times = np.linspace(-window_size, 0.0, len(amp))
 
         # Plot audio amplitude.
         self.line.set_data(times, amp)

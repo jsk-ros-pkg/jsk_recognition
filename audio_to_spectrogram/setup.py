a11e4f0c5ee2a25d88e8605810b222de61da83c6
iory <ab.ioryz@gmail.com>
2022-01-07 09:14:35 +0900

    [audio_to_spectogram] Add catkin_python_setup

---
+++ audio_to_spectrogram/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages
+from setuptools import setup
+
+from catkin_pkg.python_setup import generate_distutils_setup
+
+
+d = generate_distutils_setup(
+    packages=['audio_to_spectrogram'],
+    package_dir={'': 'src'},
+)
+
+setup(**d)

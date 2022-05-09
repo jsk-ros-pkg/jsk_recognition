794f16c88220842b047900fbcecdfd2a0609ce1b
iory <ab.ioryz@gmail.com>
2022-05-09 09:08:17 +0900

    [jsk_recognition_utils] Import tf2_ros and tf2_geometry_msgs

--- jsk_recognition_utils/python/jsk_recognition_utils/tf_compat.py
+++ jsk_recognition_utils/python/jsk_recognition_utils/tf_compat.py
@@ -6,6 +6,8 @@ import sys
 # tf_python3 import
 if os.environ['ROS_PYTHON_VERSION'] == '3':
     tf = importlib.import_module("tf")
+    tf2_ros = importlib.import_module("tf2_ros")
+    tf2_geometry_msgs = importlib.import_module("tf2_geometry_msgs")
 else:
     ws_python3_paths = [p for p in sys.path if 'devel/lib/python3' in p]
     if len(ws_python3_paths) == 0:
@@ -21,10 +23,16 @@ else:
                 os.getenv('ROS_DISTRO'))
             sys.path = [opt_python3_path] + sys.path
             tf = importlib.import_module("tf")
+            tf2_ros = importlib.import_module("tf2_ros")
+            tf2_geometry_msgs = importlib.import_module("tf2_geometry_msgs")
             sys.path.remove(opt_python3_path)
         else:
             sys.path = [ws_python3_paths[0]] + sys.path
             tf = importlib.import_module("tf")
+            tf2_ros = importlib.import_module("tf2_ros")
+            tf2_geometry_msgs = importlib.import_module("tf2_geometry_msgs")
             sys.path.remove(ws_python3_paths[0])
     else:
         tf = importlib.import_module("tf")
+        tf2_ros = importlib.import_module("tf2_ros")
+        tf2_geometry_msgs = importlib.import_module("tf2_geometry_msgs")

6b6f0f35f52c26d647a27efcdace1aba25f8c8dc
Yoshiki Obinata <mqcmd196@hotmail.co.jp>
2022-05-29 23:29:36 +0900

    [container_occupancy_detector]initial commit

---
+++ jsk_pcl_ros/include/jsk_pcl_ros/container_occupancy_detector.h
@@ -0,0 +1,104 @@
+// -*- mode: C++ -*-
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2022, JSK Lab
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/o2r other materials provided
+ *     with the distribution.
+ *   * Neither the name of the JSK Lab nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+/*
+ * container_occupancy_nodelet.h
+ * Author: Yoshiki Obinata <obinata@jsk.imi.i.u-tokyo.ac.jp>
+ */
+
+
+#ifndef CONTAINER_OCCUPANCY_NODELET_H_
+#define CONTAINER_OCCUPANCY_NODELET_H_
+
+#include <pcl_ros/pcl_nodelet.h>
+#include <message_filters/time_synchronizer.h>
+#include <message_filters/synchronizer.h>
+
+#include "jsk_recognition_utils/pcl_util.h"
+#include "jsk_recognition_utils/geo_util.h"
+#include <jsk_recognition_msgs/BoundingBoxArray.h>
+#include <jsk_topic_tools/diagnostic_nodelet.h>
+#include <sensor_msgs/PointCloud2.h>
+
+namespace jsk_pcl_ros
+{
+  class ContainerOccupancyDetector: public jsk_topic_tools::DiagnosticNodelet
+  {
+  public:
+    typedef message_filters::sync_policies::ExactTime<
+      jsk_recognition_msgs::BoundingBoxArray,
+      sensor_msgs::PointCloud2
+      > SyncPolicy;
+    ContainerOccupancyDetector() : DiagnosticNodelet("ContainerOccupancyDetector") {}
+
+  protected:
+    ////////////////////////////////////////////////////////
+    // methods
+    ////////////////////////////////////////////////////////
+    virtual void onInit();
+    virtual void calculate(
+      const jsk_recognition_msgs::BoundingBoxArray::ConstPtr& box_array_msg,
+      const sensor_msgs::PointCloud2::ConstPtr& points_msg);
+    virtual void updateDiagnostic(
+      diagnostic_updater::DiagnosticStatusWrapper &stat);
+    virtual void subscribe();
+    virtual void unsubscribe();
+
+    ////////////////////////////////////////////////////////
+    // ROS varariables
+    ////////////////////////////////////////////////////////
+    message_filters::Subscriber<jsk_recognition_msgs::BoundingBoxArray> sub_box_;
+    message_filters::Subscriber<sensor_msgs::PointCloud2> sub_points_;
+    std::shared_ptr<message_filters::Synchronizer<SyncPolicy> >sync_;
+    ros::Publisher occupancy_rate_pub_;
+    boost::mutex mutex_;
+
+    ////////////////////////////////////////////////////////
+    // Diagnostics Variables
+    ////////////////////////////////////////////////////////
+    jsk_recognition_utils::Counter remove_counter_;
+    jsk_recognition_utils::Counter pass_counter_;
+
+    ////////////////////////////////////////////////////////
+    // Parameters
+    ////////////////////////////////////////////////////////
+
+    bool with_indices_;
+
+  private:
+
+  };
+}
+
+#endif // CONTAINER_OCCUPANCY_NODELET_H_

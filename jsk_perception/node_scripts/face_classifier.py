20fa251b61c1ce1694e34bdbee876cbb5da0cf08
Koki Shinjo <sktometometo@gmail.com>
2022-11-24 16:05:56 +0900

    [jsk_perception] add publisher to face_classifier.py

--- jsk_perception/node_scripts/face_classifier.py
+++ jsk_perception/node_scripts/face_classifier.py
@@ -2,6 +2,11 @@
 
 from jsk_recognition_utils.face_recognizer import FaceRecognizer
 
+from jsk_recognition_msgs.msg import Rect
+from jsk_recognition_msgs.msg import RectArray
+from jsk_recognition_msgs.msg import Label
+from jsk_recognition_msgs.msg import LabelArray
+
 import rospy
 import cv_bridge
 from sensor_msgs.msg import Image
@@ -13,22 +18,36 @@ class FaceClassifier:
 
         known_person_image_dir = rospy.get_param('~known_person_image_dir')
         fitting_method = rospy.get_param('~fitting_method', 'svm')
-        self.recognizer = FaceRecognizer(known_person_image_dir, fitting_method)
+        self.recognizer = FaceRecognizer(
+            known_person_image_dir, fitting_method)
 
         self.cv_bridge = cv_bridge.CvBridge()
 
-        self.sub = rospy.Subscribe('~input', Image, self,callback)
+        self.sub = rospy.Subscriber('~input', Image, self, callback)
+
+        self.pub_rects = rospy.Publisher(
+            '~output/rects', RectArray, queue_size=1)
+        self.pub_labels = rospy.Publisher(
+            '~output/labels', LabelsArray, queue_size=1)
 
     def callback(self, msg):
 
         image = self.cv_bridge.imgmsg_to_cv2(msg)
         names, locations = self.recognizer(image)
-        rospy.loginfo('names: {}'.format(names))
-        rospy.loginfo('locations: {}'.format(locations))
+
+        msg_rects = RectArray()
+        msg_rects.header = msg.header
+        msg_rects.rects = [Rect(x=loc[0], y=loc[1], width=loc[2]-loc[0], height=loc[3]-loc[1] for loc in locations]
+        msg_labels=LabelArray()
+        msg_labels.header = msg.header
+        msg_labels.labels = [Label(id=-1, name=name) for name in names]
+
+        self.pub_rects.publish(msg_rects)
+        self.pub_labels.publish(msg_labels)
 
 
 if __name__ == '__main__':
 
     rospy.init_node('face_classifier')
-    node = FaceClassifier()
+    node=FaceClassifier()
     rospy.spin()

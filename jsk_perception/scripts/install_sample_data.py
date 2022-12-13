e732c8dc218ad605b5c1383370b89c5138476c0a
Shingo Kitagawa <shingogo@hotmail.co.jp>
2022-12-13 19:26:37 +0900

    upda rosbag panorama human 3d detection

--- jsk_perception/scripts/install_sample_data.py
+++ jsk_perception/scripts/install_sample_data.py
@@ -120,9 +120,9 @@ def main():
 
     download_data(
         pkg_name=PKG,
-        path='sample/data/sample_rect_array_in_panorama_to_bounding_box_array.bag',
-        url='https://drive.google.com/uc?id=1f613TsYuPk1DWuGuUfkJVBbPv2-4Io69',
-        md5='04ba99ee993860634c0064d167b5cbe5',
+        path='sample/data/sample_panorama_human_3d_detection.bag',
+        url='https://drive.google.com/uc?id=1AqptBNBG5_fF9nYPlAC-ZoRr7exsD5w7',
+        md5='fdf8839b03305daa2c0ddbce02a1764a',
         extract=False,
     )
 

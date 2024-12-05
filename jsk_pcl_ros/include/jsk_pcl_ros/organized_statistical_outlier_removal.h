675971f392f08daef1455a253ebfea07732cbfc5
GITAI <admin@gitai.tech>
2024-12-04 17:20:25 -0800

    work around for bug in  extract indices

--- jsk_pcl_ros/include/jsk_pcl_ros/organized_statistical_outlier_removal.h
+++ jsk_pcl_ros/include/jsk_pcl_ros/organized_statistical_outlier_removal.h
@@ -78,6 +78,10 @@ namespace jsk_pcl_ros
 
     void filter(const pcl::PCLPointCloud2::Ptr pcl_cloud,
                 pcl::PointIndices::Ptr pcl_indices_filtered);
+    void filter(const pcl::PCLPointCloud2::Ptr pcl_cloud,
+                const pcl::PointIndices::Ptr pcl_indices,
+                pcl::PointIndices::Ptr pcl_indices_filtered);
+    std::vector<int> getFilteredIndices(const pcl::PointCloud<pcl::PointXYZ>::Ptr cloud);
     void filterCloud(const sensor_msgs::PointCloud2::ConstPtr& msg);
     void filterCloudWithClusterPointIndices(
             const sensor_msgs::PointCloud2::ConstPtr& msg,
